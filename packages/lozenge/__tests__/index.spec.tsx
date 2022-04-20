c18d6c88a920923d79541b946b674fed2d3bf7e3
Robert Niznik <rniznik@launchdarkly.com>
2022-04-20 10:53:38 -0400

    fix: add describe

--- packages/lozenge/__tests__/index.spec.tsx
+++ packages/lozenge/__tests__/index.spec.tsx
@@ -1,16 +1,18 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { axe } from '../../../tests/utils';
 
 import { Lozenge } from '../src';
 
-it('renders in the loading state', () => {
-  render(<Lozenge>hi</Lozenge>);
-  expect(screen.getByText('hi')).toBeInTheDocument();
-});
+describe('Lozenge', () => {
+  it('renders', () => {
+    render(<Lozenge>hi</Lozenge>);
+    expect(screen.getByText('hi')).toBeInTheDocument();
+  });
 
-it('is accessible', async () => {
-  const { container } = render(<Lozenge>hi</Lozenge>);
-  const results = await axe(container);
-  expect(results).toHaveNoViolations();
+  it('is accessible', async () => {
+    const { container } = render(<Lozenge>hi</Lozenge>);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
 });
