6ffe4b5612b5c9231824b6e3a490844b29084f54
Robert Niznik <rniznik@launchdarkly.com>
2022-04-20 10:48:01 -0400

    ci: run unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./tests/setupTests.ts'],
+    setupFiles: [path.resolve(__dirname, './tests/setupTests.ts')],
     coverage: {
       lines: 50,
       functions: 50,
