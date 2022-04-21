0beaaba6529db77d2468858b1bbc78992529a0c7
Robert Niznik <rniznik@launchdarkly.com>
2022-04-21 09:41:39 -0400

    ci: add changes to test

--- packages/lozenge/src/index.tsx
+++ packages/lozenge/src/index.tsx
@@ -1,7 +1,10 @@
 import type { ReactNode } from 'react';
 
 const Lozenge = ({ children }: { children: ReactNode }) => (
-  <span style={{ background: 'lightcyan' }}>{children}</span>
+  <>
+    <span style={{ background: 'lightcyan' }}>{children}</span>
+    <div style={{ background: 'pink' }}>test</div>
+  </>
 );
 
 export { Lozenge };
