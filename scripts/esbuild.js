c81d6a380aa06a5a754fd8b2293691390aa3ef5b
Robert Niznik <rniznik@launchdarkly.com>
2022-04-27 16:07:23 -0400

    fix: build css separately

--- scripts/esbuild.js
+++ scripts/esbuild.js
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const esbuild = require('esbuild');
 const path = require('path');
+const fg = require('fast-glob');
 
 const build = async (packageJSON) => {
   const outFileMain = packageJSON.main;
   const outFileModule = packageJSON.module;
   const entryPoints = [packageJSON.source];
+  const cssPaths = await fg([`${path.resolve()}/src/**/*.css`]);
 
   const config = {
     bundle: true,
@@ -16,6 +18,7 @@ const build = async (packageJSON) => {
     external: [
       ...Object.keys(packageJSON.dependencies || {}),
       ...Object.keys(packageJSON.peerDependencies || {}),
+      '*.css',
     ],
     inject: [path.resolve(__dirname, './react-shim.js')],
   };
@@ -33,6 +36,15 @@ const build = async (packageJSON) => {
       ...config,
       format: 'esm',
       outfile: outFileModule,
+    })
+    .catch(() => process.exit(1));
+
+  await esbuild
+    .build({
+      bundle: true,
+      entryPoints: cssPaths,
+      sourcemap: true,
+      outdir: 'dist',
     })
     .catch(() => process.exit(1));
 };
