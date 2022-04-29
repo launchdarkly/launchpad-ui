99bebeafa488565f19bfb02299576072b762a5a6
Robert Niznik <rniznik@launchdarkly.com>
2022-04-29 09:36:38 -0400

    ci: add script to build and test

--- packages/lozenge/__tests__/index.e2e.ts
+++ packages/lozenge/__tests__/index.e2e.ts
@@ -1,9 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('is focusable when clickable', async ({ page }) => {
-  await page.goto(
-    'http://localhost:6006/iframe.html?id=components-lozenge--default&viewMode=story&args=isClickable:true'
-  );
+  await page.goto('?id=components-lozenge--default&viewMode=story&args=isClickable:true');
   const lozenge = page.locator('.Lozenge');
   await lozenge.click();
   await expect(lozenge).toBeFocused();
--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,9 @@ const config: PlaywrightTestConfig = {
   testMatch: /.*\.e2e\.ts/,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
+  reporter: process.env.CI ? 'github' : 'list',
   use: {
+    baseURL: 'http://localhost:6006/iframe.html',
     trace: 'on-first-retry',
   },
   projects: [
