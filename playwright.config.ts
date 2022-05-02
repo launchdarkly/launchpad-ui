68f3c658e38758a622935c554b52262f89e8c4fe
Robert Niznik <rniznik@launchdarkly.com>
2022-05-02 14:28:05 -0400

    feat: add Playwright for e2e tests (#19)
    * feat: add playwright for e2e tests
    
    * ci: add script to build and test
    
    * feat: add watch mode
    
    * ci: add e2e job
    
    * fix: install playwright deps
    
    * ci: bump workers
    
    * ci: cache browsers
    
    * fix: spacing
    
    * ci: install deps
    
    * ci: update scripts

---
+++ playwright.config.ts
@@ -0,0 +1,29 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testMatch: /.*\.e2e\.ts/,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: process.env.CI ? 'github' : 'list',
+  workers: process.env.CI ? 2 : undefined,
+  use: {
+    baseURL: 'http://localhost:6006/iframe.html',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
+
+export default config;
