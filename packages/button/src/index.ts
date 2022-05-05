e93670c6e8fdcdefb919e259d28284827795f6f0
Robert Niznik <rniznik@launchdarkly.com>
2022-05-05 09:04:32 -0400

    chore: cleanup

--- packages/button/src/index.ts
+++ packages/button/src/index.ts
@@ -1,19 +1,4 @@
 export type { ButtonProps } from './Button';
-// export type { SplitButtonProps } from './SplitButton';
 
 export { Button } from './Button';
 export { ButtonGroupSpacing, ButtonKind, ButtonSize, ButtonType } from './types';
-// export { ButtonGroup } from './ButtonGroup';
-// export { PrimaryButton } from './PrimaryButton';
-// export { DestructiveButton } from './DestructiveButton';
-// export { SubmitButton } from './SubmitButton';
-// export { CopyButton } from './CopyButton';
-// export {
-//   EllipsisButton,
-//   EllipsisButtonWithDropdown,
-//   VerticalEllipsisButton,
-// } from './EllipsisButton';
-// export { LinkButton } from './LinkButton';
-// export { SplitButton } from './SplitButton';
-// export { UploadButton } from './UploadButton';
-// export { UploadLink } from './UploadLink';
