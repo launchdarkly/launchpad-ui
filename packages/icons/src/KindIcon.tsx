fab9f06ad3cacc5298c632b58229fefd9474e911
Robert Niznik <rniznik@launchdarkly.com>
2022-05-17 09:54:02 -0400

    feat(icon): add KindIcon

---
+++ packages/icons/src/KindIcon.tsx
@@ -0,0 +1,40 @@
+import { Component } from 'react';
+
+import { CheckCircle } from './CheckCircle';
+import { ErrorCircle } from './ErrorCircle';
+import { Info } from './Info';
+import { Warning } from './Warning';
+import { IconSize } from './types';
+
+type KindIconProps = {
+  kind: 'info' | 'success' | 'warning' | 'error' | 'striped';
+  size?: IconSize;
+  className?: string;
+};
+
+export class KindIcon extends Component<KindIconProps> {
+  render() {
+    const { kind, size, className } = this.props;
+    const iconSize = size ? size : IconSize.MEDIUM;
+    let body;
+
+    switch (kind) {
+      case 'success':
+        body = <CheckCircle size={iconSize} />;
+        break;
+      case 'warning':
+      case 'striped':
+        body = <Warning size={iconSize} />;
+        break;
+      case 'error':
+        body = <ErrorCircle size={iconSize} />;
+        break;
+      case 'info':
+      default:
+        body = <Info size={iconSize} />;
+        break;
+    }
+
+    return <span className={className}>{body}</span>;
+  }
+}
