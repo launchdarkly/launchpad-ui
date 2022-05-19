3d5fe5d76cc436d0977bc72f2cab42d96f77cf30
Robert Niznik <rniznik@launchdarkly.com>
2022-05-19 16:28:07 -0400

    feat: add progress

---
+++ packages/progress/src/index.ts
@@ -0,0 +1,5 @@
+export type { DelayedIndicatorProps } from './DelayedIndicator';
+export type { ProgressProps } from './Progress';
+export { DelayedIndicator } from './DelayedIndicator';
+export { Progress, ProgressSize } from './Progress';
+export { AnimationDelay } from './types';
