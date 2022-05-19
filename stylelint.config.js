5c07ee72e5e32bbe7160b1e1bfc502708649575b
Robert Niznik <rniznik@launchdarkly.com>
2022-05-19 09:44:56 -0400

    fix: update unknown custom properties

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const fg = require('fast-glob');
+
+let cssPaths = [];
+(async () => {
+  cssPaths = await fg([path.resolve(__dirname, `packages/**/src/**/*.css`), '!**/node_modules']);
+})();
+
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-value-no-unknown-custom-properties'],
   rules: {
     'declaration-no-important': true,
     'custom-property-pattern': null,
@@ -7,5 +17,11 @@ module.exports = {
     'declaration-block-no-duplicate-custom-properties': null,
     'hue-degree-notation': 'number',
     'alpha-value-notation': 'number',
+    'csstools/value-no-unknown-custom-properties': [
+      true,
+      {
+        importFrom: [path.resolve(__dirname, 'packages/tokens/dist/index.css'), ...cssPaths],
+      },
+    ],
   },
 };
