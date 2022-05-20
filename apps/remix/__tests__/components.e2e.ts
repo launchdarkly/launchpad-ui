fb973f826d80c2cad8a6526d89eb8636bd3aec3c
Robert Niznik <rniznik@launchdarkly.com>
2022-05-20 09:45:47 -0400

    refactor: automate ssr tests

--- apps/remix/__tests__/components.e2e.ts
+++ apps/remix/__tests__/components.e2e.ts
@@ -1,39 +1,29 @@
+import type { Page } from '@playwright/test';
 import { test, expect } from '@playwright/test';
 
+import { getComponents } from '~/data.server';
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
+});
+
+test.afterAll(async () => {
+  await page.close();
+});
+
 test.describe('Remix SSR', () => {
-  test('alert renders', async ({ page }) => {
-    await page.goto('/components/alert');
-    const alert = page.locator('.Alert');
-    await expect(alert).toBeVisible();
-  });
+  test('components render', async () => {
+    const components = await getComponents();
 
-  test('button renders', async ({ page }) => {
-    await page.goto('/components/button');
-    const button = page.locator('.Button');
-    await expect(button).toBeVisible();
-  });
-
-  test('icon renders', async ({ page }) => {
-    await page.goto('/components/icon');
-    const icon = page.locator('.Icon');
-    await expect(icon).toBeVisible();
-  });
-
-  test('lozenge renders', async ({ page }) => {
-    await page.goto('/components/lozenge');
-    const lozenge = page.locator('.Lozenge');
-    await expect(lozenge).toBeVisible();
-  });
-
-  test('progress renders', async ({ page }) => {
-    await page.goto('/components/progress');
-    const progress = page.locator('.Progress');
-    await expect(progress).toBeVisible();
-  });
-
-  test('toggle renders', async ({ page }) => {
-    await page.goto('/components/toggle');
-    const toggle = page.locator('.Toggle');
-    await expect(toggle).toBeVisible();
+    for (const component of components) {
+      // eslint-disable-next-line no-loop-func
+      await test.step(`${component.name} renders`, async () => {
+        await page.goto(`/${component.to}`);
+        const element = page.locator(`.${component.name}`);
+        await expect(element).toBeVisible();
+      });
+    }
   });
 });
--- apps/remix/app/routes/index.tsx
+++ apps/remix/app/routes/index.tsx
@@ -2,41 +2,15 @@ import type { LoaderFunction, MetaFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
 
-type IndexData = {
-  components: Array<{ name: string; to: string }>;
-};
+import type { Component } from '~/data.server';
+import { getComponents } from '~/data.server';
+
+type LoaderData = { components: Array<Component> };
 
 export const loader: LoaderFunction = async () => {
-  const data: IndexData = {
-    components: [
-      {
-        to: 'components/alert',
-        name: 'Alert',
-      },
-      {
-        to: 'components/button',
-        name: 'Button',
-      },
-      {
-        to: 'components/icon',
-        name: 'Icon',
-      },
-      {
-        to: 'components/lozenge',
-        name: 'Lozenge',
-      },
-      {
-        to: 'components/progress',
-        name: 'Progress',
-      },
-      {
-        to: 'components/toggle',
-        name: 'Toggle',
-      },
-    ],
-  };
-
-  return json(data);
+  return json<LoaderData>({
+    components: await getComponents(),
+  });
 };
 
 export const meta: MetaFunction = () => {
@@ -47,7 +21,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const data = useLoaderData<IndexData>();
+  const data = useLoaderData<LoaderData>();
 
   return (
     <>
