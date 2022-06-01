b422e23f638213318e129940c2cb243bd6db2372
Robert Niznik <rniznik@launchdarkly.com>
2022-06-01 16:37:37 -0400

    feat: add sheet story

--- packages/modal/stories/Modal.stories.tsx
+++ packages/modal/stories/Modal.stories.tsx
@@ -2,11 +2,11 @@
 import { Button } from '@launchpad-ui/button';
 import { useState } from '@storybook/client-api';
 
-import { ModalBody, ModalFooter, ModalHeader, ModalTransition, Prompt } from '../src';
+import { ModalBody, ModalFooter, ModalHeader, ModalSheet, ModalTransition, Prompt } from '../src';
 
 export default {
   component: ModalTransition,
-  subcomponents: { ModalBody, ModalFooter, ModalHeader, Prompt },
+  subcomponents: { ModalBody, ModalFooter, ModalHeader, ModalSheet, Prompt },
   title: 'Components/Modal',
   description: 'Modals presents users information and actions over a page.',
   parameters: {
@@ -46,3 +46,26 @@ export const Default = {
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 Default.parameters = { docs: { disable: true } };
+
+export const Sheet = {
+  render: () => {
+    const [show, setShow] = useState(true);
+    const button = <Button onClick={() => setShow(true)}>Open modal</Button>;
+    return show ? (
+      <>
+        {button}
+
+        <ModalSheet withCloseButton onCancel={() => setShow(!show)}>
+          <section>
+            <ModalHeader>Example modal title</ModalHeader>
+          </section>
+        </ModalSheet>
+      </>
+    ) : (
+      button
+    );
+  },
+};
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+Sheet.parameters = { docs: { disable: true } };
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('postcss-nested'),
     require('postcss-preset-env')({ browsers: 'last 2 versions, not dead, not IE 11' }),
   ],
