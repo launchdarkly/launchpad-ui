7f007a05953765b62d1e3ee9c3ac4c9acd78c1a1
Robert Niznik <rniznik@launchdarkly.com>
2022-06-02 16:21:07 -0400

    feat: add stories

---
+++ packages/popover/stories/Popover.stories.tsx
@@ -0,0 +1,57 @@
+import { Button } from '@launchpad-ui/button';
+import { userEvent, within } from '@storybook/testing-library';
+
+import { Popover } from '../src';
+
+export default {
+  component: Popover,
+  title: 'Components/Popover',
+  description: 'Popovers display content within a portal triggered by user interactions.',
+  argTypes: {
+    placement: {
+      table: {
+        category: 'Presentation',
+      },
+    },
+    interactionKind: {
+      table: {
+        category: 'Presentation',
+      },
+    },
+    children: {
+      table: {
+        category: 'Content',
+      },
+    },
+    onClick: {
+      table: {
+        category: 'Functions',
+      },
+    },
+    onClose: {
+      table: {
+        category: 'Functions',
+      },
+    },
+    onInteraction: {
+      table: {
+        category: 'Functions',
+      },
+    },
+  },
+};
+
+export const Default = {
+  args: {
+    children: [
+      // eslint-disable-next-line react/jsx-key
+      <Button>Target</Button>,
+      // eslint-disable-next-line react/jsx-key
+      <div style={{ padding: '2rem' }}>Content to show</div>,
+    ],
+  },
+  play: async ({ canvasElement }: { canvasElement: HTMLElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('button'));
+  },
+};
