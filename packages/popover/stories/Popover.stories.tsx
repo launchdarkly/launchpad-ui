8482d19edd2900b59614387be3cd9fb81ed9aa7b
Robert Niznik <rniznik@launchdarkly.com>
2022-06-02 16:44:55 -0400

    fix: add decorator

--- packages/popover/stories/Popover.stories.tsx
+++ packages/popover/stories/Popover.stories.tsx
@@ -1,3 +1,5 @@
+import type { Story } from '@storybook/react';
+
 import { Button } from '@launchpad-ui/button';
 import { userEvent, within } from '@storybook/testing-library';
 
@@ -39,6 +41,21 @@ export default {
       },
     },
   },
+  decorators: [
+    (Story: Story) => (
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          display: 'grid',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export const Default = {
