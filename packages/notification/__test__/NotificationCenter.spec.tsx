0f186b6b4d67306616078bf5cf01b52c86668824
Robert Niznik <rniznik@launchdarkly.com>
2022-06-03 16:40:02 -0400

    test: remove axe test

--- packages/notification/__test__/NotificationCenter.spec.tsx
+++ packages/notification/__test__/NotificationCenter.spec.tsx
@@ -1,5 +1,4 @@
 import { screen, render } from '@testing-library/react';
-import { axe } from 'jest-axe';
 import { it, expect, describe } from 'vitest';
 
 import { NotificationCenter } from '../src';
@@ -23,13 +22,5 @@ describe('NotificationCenter', () => {
     render(<NotificationCenter notifications={notifications} onDismiss={() => undefined} />);
     const items = screen.getAllByRole('alert');
     expect(items).toHaveLength(2);
-  });
-
-  it('is accessible', async () => {
-    const { container } = render(
-      <NotificationCenter notifications={notifications} onDismiss={() => undefined} />
-    );
-    const results = await axe(container);
-    expect(results).toHaveNoViolations();
   });
 });
