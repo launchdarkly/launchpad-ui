d6f8955a326cf538ecae23fbc7a3125a62760b6d
Chase Wackerfuss <cwackerfuss@launchdarkly.com>
2022-06-03 15:14:28 -0500

    chore(notification): improve test coverage for notificationcenter

--- packages/notification/__test__/NotificationCenter.spec.tsx
+++ packages/notification/__test__/NotificationCenter.spec.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable testing-library/no-node-access */
 import { screen, render } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import { it, expect, describe } from 'vitest';
 
 import { NotificationCenter } from '../src';
@@ -23,5 +24,13 @@ describe('notification', () => {
     render(<NotificationCenter notifications={notifications} onDismiss={() => undefined} />);
     const items = screen.getAllByRole('alert');
     expect(items).toHaveLength(2);
+  });
+
+  it('is accessible', async () => {
+    const { container } = render(
+      <NotificationCenter notifications={notifications} onDismiss={() => undefined} />
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
