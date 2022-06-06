a1c653182f1f6706bc3be6b23c329b3cf736bf0f
Robert Niznik <rniznik@launchdarkly.com>
2022-06-06 10:12:06 -0400

    feat: add banner package

---
+++ packages/banner/__tests__/Banner.spec.tsx
@@ -0,0 +1,28 @@
+import { AlertKind } from '@launchpad-ui/alert';
+import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
+import { it, expect, describe } from 'vitest';
+
+import { Banner } from '../src';
+
+describe('Banner', () => {
+  it('renders', () => {
+    render(<Banner kind={AlertKind.INFO}>An important message</Banner>);
+    expect(screen.getByText('An important message')).toBeInTheDocument();
+  });
+
+  it('is accessible', async () => {
+    const { container } = render(<Banner kind={AlertKind.INFO}>An important message</Banner>);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
+  });
+
+  it('can be dismissible', async () => {
+    render(
+      <Banner kind={AlertKind.INFO} dismissible>
+        An important message
+      </Banner>
+    );
+    expect(screen.getByRole('button')).toBeVisible();
+  });
+});
