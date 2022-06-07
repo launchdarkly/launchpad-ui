fe3d984a6cb027435202a16d4d45ea5e5403bf28
Robert Niznik <rniznik@launchdarkly.com>
2022-06-07 09:39:10 -0400

    fix(remix): update config

--- apps/remix/remix.config.js
+++ apps/remix/remix.config.js
@@ -7,5 +7,5 @@ module.exports = {
   assetsBuildDirectory: 'public/build',
   serverBuildPath: 'build/index.js',
   publicPath: '/build/',
-  serverDependenciesToBundle: [/^@launchpad-ui.*/],
+  serverDependenciesToBundle: [/^@launchpad-ui.*/, 'lodash-es'],
 };
