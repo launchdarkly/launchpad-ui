7a6ff80153b7d5b6fab393ae77515f495e8bc865
Robert Niznik <rniznik@launchdarkly.com>
2022-06-08 08:39:22 -0400

    feat: add tests

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,7 +1,10 @@
 import '@testing-library/jest-dom';
+import { configure } from '@testing-library/react';
 import { toHaveNoViolations } from 'jest-axe';
 import { vi } from 'vitest';
 
 expect.extend(toHaveNoViolations);
 
 vi.stubGlobal('scroll', vi.fn());
+
+configure({ testIdAttribute: 'data-test-id' });
