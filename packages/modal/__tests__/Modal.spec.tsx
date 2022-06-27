f228e6dc0ab5eeab5236e15a71c85b683c2de1e7
Robert Niznik <rniznik@launchdarkly.com>
2022-06-27 09:51:28 -0700

    test(modal): add a11y test

--- packages/modal/__tests__/Modal.spec.tsx
+++ packages/modal/__tests__/Modal.spec.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { axe } from 'jest-axe';
 import { it, expect, describe } from 'vitest';
 
 import { Modal, ModalBody, ModalFooter, ModalHeader, Prompt } from '../src';
@@ -15,5 +16,19 @@ describe('Modal', () => {
       </Prompt>
     );
     expect(await screen.findByRole('dialog')).toBeInTheDocument();
+  });
+
+  it('is accessible', async () => {
+    const { container } = render(
+      <Prompt>
+        <Modal transition="pop" withCloseButton>
+          <ModalHeader>Modal</ModalHeader>
+          <ModalBody>Body</ModalBody>
+          <ModalFooter>Footer</ModalFooter>
+        </Modal>
+      </Prompt>
+    );
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
