6a5987fcacd2ed3759e716e718a89e842e87b683
Robert Niznik <rniznik@launchdarkly.com>
2022-07-15 08:47:55 -0400

    ci: test upload

--- packages/form/__tests__/Form.e2e.tsx
+++ packages/form/__tests__/Form.e2e.tsx
@@ -34,9 +34,7 @@ test.describe('Form', () => {
         <CompactTextField label="Email" aria-label="Email" onChange={() => undefined} />
         <h4>Icon Field</h4>
         <IconField icon={Info}>
-          <FormField isRequired={true} label="Date" name="Date" htmlFor="Date">
-            <TextField id="Date" value="12/01/2022" />
-          </FormField>
+          <TextField id="Date" value="12/01/2022" />
         </IconField>
         <h4>Select</h4>
         <Select name="select" aria-label="My select" value="one" onChange={() => undefined}>
