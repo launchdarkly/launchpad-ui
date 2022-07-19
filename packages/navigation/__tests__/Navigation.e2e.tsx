7b8ff130180564bda51648f8c171f988d385fe96
Robert Niznik <rniznik@launchdarkly.com>
2022-07-19 10:35:56 -0400

    feat: add e2e tests

--- packages/navigation/__tests__/Navigation.e2e.tsx
+++ packages/navigation/__tests__/Navigation.e2e.tsx
@@ -1,43 +1,48 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { Navigation, NavigationItem, type NavigationItemProps } from '../src/Navigation';
+import {
+  Navigation,
+  NavigationItem,
+  type NavigationProps,
+  type NavigationItemProps,
+} from '../src/Navigation';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-const createComponent = (items: NavigationItemProps[]) => (
+const createComponent = (props?: NavigationProps<NavigationItemProps>) => (
   <MemoryRouter>
-    <Navigation items={items} title="Page menu">
-      {(item) => (
-        <NavigationItem
-          key={item.to}
-          name={item.name}
-          to={item.to}
-          tooltip={item.tooltip}
-          tooltipContent={item.tooltipContent}
-          status={item.status}
-        />
-      )}
+    <Navigation title="Page menu" {...props}>
+      <NavigationItem key={1} name="First" to="/first" />
+      <NavigationItem key={2} name="Second" to="/second" />
+      <NavigationItem key={3} name="Third" to="/third" />
+      <NavigationItem key={4} name="Fourth" to="/fourth" />
     </Navigation>
   </MemoryRouter>
 );
 
 test.describe('Navigation', () => {
   test('is accessible', async ({ mount, page }) => {
-    const component = await mount(
-      createComponent([
-        {
-          name: 'First',
-          to: '/first',
-        },
-        {
-          name: 'Second',
-          to: '/second',
-        },
-      ])
-    );
+    const component = await mount(createComponent());
 
     await expect(component).toBeVisible();
+    await expect(page).toBeAccessible();
+  });
+
+  test('renders as a dropdown menu when viewport is small', async ({ mount, page }) => {
+    await mount(createComponent());
+
+    await page.setViewportSize({
+      width: 320,
+      height: 500,
+    });
+
+    await page.locator('text=Page menu').click();
+    // skip animations
+    await page.locator('.Popover-content').evaluate((node) => (node.style.opacity = '1'));
+
+    const element = page.locator('[role="menu"]');
+    await expect(element).toBeVisible();
     await expect(page).toBeAccessible();
   });
 });
