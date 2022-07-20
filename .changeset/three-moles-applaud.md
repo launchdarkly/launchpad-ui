83a1cb5dc791fa5aecd5e829bd001065035d98cf
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-07-20 13:06:58 -0500

    feat(markdown): add markdown package

---
+++ .changeset/three-moles-applaud.md
@@ -0,0 +1,8 @@
+---
+'@launchpad-ui/markdown': minor
+'@launchpad-ui/core': patch
+---
+
+Add markdown package:
+
+- [Markdown] Add markdown code, stories, and tests
--- apps/remix/app/routes/components/markdown.tsx
+++ apps/remix/app/routes/components/markdown.tsx
@@ -1,5 +1,16 @@
 import { Markdown } from '@launchpad-ui/core';
 
+const SAMPLE_MARKDOWN = `
+This is markdown content.
+  * Added the variation \`"abc"\`
+  * Added the variation \`"cde"\`
+  * Removed the variation ~~\`"abc"\`~~
+  * Removed the variation ~~\`"cde"\`~~
+  * Removed the variation ~~\`"foo"\`~~
+
+[External links](https://launchdarkly.com) always open in a new tab, but [relative links](/?path=/story/intro--page) and [links that start with the baseUri parameter]("https://app.launchdarkly.com/settings") do not.
+`;
+
 export default function Index() {
-  return <Markdown>A lovely Markdown component.</Markdown>;
+  return <Markdown source={SAMPLE_MARKDOWN} />;
 }
