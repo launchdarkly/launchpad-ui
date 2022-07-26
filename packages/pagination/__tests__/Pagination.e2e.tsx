a8141b3350e41ea5e8047b89b657c55846c8f161
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-07-26 14:46:02 -0500

    feat(pagination): fix unit tests for pagination

--- packages/pagination/__tests__/Pagination.e2e.tsx
+++ packages/pagination/__tests__/Pagination.e2e.tsx
@@ -4,13 +4,22 @@ import { Pagination, type PaginationProps } from '../src/Pagination';
 
 test.use({ viewport: { width: 500, height: 500 } });
 
-const createComponent = (props?: PaginationProps) => (
+const createComponent = (props: PaginationProps) => (
   <Pagination {...props}>An important message</Pagination>
 );
 
 test.describe('Pagination', () => {
   test('is accessible', async ({ mount, page }) => {
-    const component = await mount(createComponent());
+    const component = await mount(
+      createComponent({
+        resourceName: 'rocketship',
+        onChange: () => undefined,
+        currentOffset: 0,
+        pageSize: 2,
+        isReady: true,
+        totalCount: 4,
+      })
+    );
 
     await expect(component).toBeVisible();
     await expect(page).toBeAccessible();
--- packages/pagination/__tests__/Pagination.spec.tsx
+++ packages/pagination/__tests__/Pagination.spec.tsx
@@ -5,7 +5,19 @@ import { Pagination } from '../src';
 
 describe('Pagination', () => {
   it('renders', () => {
-    render(<Pagination>An important message</Pagination>);
-    expect(screen.getByText('An important message')).toBeInTheDocument();
+    render(
+      <Pagination
+        resourceName={'rocketship'}
+        onChange={() => undefined}
+        currentOffset={0}
+        pageSize={2}
+        isReady={true}
+        totalCount={4}
+      />
+    );
+    expect(screen.getByLabelText('first rocketship page')).toBeInTheDocument();
+    expect(screen.getByLabelText('previous rocketship page')).toBeInTheDocument();
+    expect(screen.getByLabelText('next rocketship page')).toBeInTheDocument();
+    expect(screen.getByLabelText('last rocketship page')).toBeInTheDocument();
   });
 });
