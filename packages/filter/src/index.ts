1573c93c0daa6edbedeadac36654280b0323e1f4
Valerie Roske <vroske@launchdarkly.com>
2022-07-26 17:20:41 -0700

    feat: add FilterButton component to filter package

--- packages/filter/src/index.ts
+++ packages/filter/src/index.ts
@@ -1,2 +1,4 @@
 export type { FilterProps } from './Filter';
+export type { FilterButtonProps } from './FilterButton';
 export { Filter } from './Filter';
+export { FilterButton } from './FilterButton';
---
+++ packages/filter/stories/FilterButton.stories.tsx
@@ -0,0 +1,36 @@
+import type { ComponentStoryObj } from '@storybook/react';
+
+import { FilterButton } from '../src';
+
+export default {
+  component: FilterButton,
+  title: 'Components/FilterButton',
+  description: 'filter a list of results',
+  parameters: {
+    status: {
+      type: process.env.PACKAGE_STATUS__FILTER,
+    },
+  },
+};
+
+type Story = ComponentStoryObj<typeof FilterButton>;
+
+export const Basic: Story = {
+  args: {
+    name: 'Filter by Name',
+  },
+};
+
+export const IconOnly: Story = {
+  args: {
+    name: 'Filter by Name',
+    hideName: true,
+  },
+};
+
+export const Clearable: Story = {
+  args: {
+    name: 'Filter by Name',
+    isClearable: true,
+  },
+};
