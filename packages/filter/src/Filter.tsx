6d472673c252c7f3dcabe4de4790317e18408448
Valerie Roske <vroske@launchdarkly.com>
2022-07-27 17:03:15 -0700

    feat: add Filter component to filter package

--- packages/filter/src/Filter.tsx
+++ packages/filter/src/Filter.tsx
@@ -1,17 +1,95 @@
+import type { FilterOption } from './FilterMenu';
+import type { MenuProps } from '@launchpad-ui/menu';
+import type { ReactNode, SyntheticEvent } from 'react';
+
+import { Dropdown } from '@launchpad-ui/dropdown';
 import cx from 'clsx';
 
-import './styles/Filter.css';
+import { FilterButton } from './FilterButton';
+import { FilterMenu } from './FilterMenu';
 
-type FilterProps = {
+const SEARCH_INPUT_THRESHOLD = 4;
+
+type FilterProps = Pick<MenuProps<string>, 'menuWidth' | 'enableVirtualization'> & {
+  searchValue?: string;
+  onSearchChange?(event: React.ChangeEvent<HTMLInputElement>): void;
+  searchPlaceholder?: string;
+  searchAriaLabel?: string;
+  name: ReactNode;
+  hideName?: boolean;
+  description: ReactNode;
+  options: FilterOption[];
+  isClearable?: boolean;
+  onClear?(): void;
   className?: string;
-  children: React.ReactNode;
+  onStateChange?({ isOpen }: { isOpen?: boolean }): void;
+  isSelected?: boolean;
+  onSelect?(item: FilterOption): void;
+  isEmpty?: boolean;
+  isLoading?: boolean;
+  onClickFilterButton?(): void;
+  disabledOptionTooltip?: string;
+  testId?: string;
 };
 
-const Filter = ({ children, className }: FilterProps) => {
+const Filter = ({
+  searchValue,
+  onSearchChange,
+  searchPlaceholder,
+  searchAriaLabel,
+  name,
+  hideName,
+  description,
+  options,
+  isClearable,
+  onClear,
+  isSelected,
+  className,
+  isEmpty,
+  isLoading,
+  onClickFilterButton,
+  disabledOptionTooltip,
+  testId,
+  menuWidth,
+  enableVirtualization,
+  ...props
+}: FilterProps) => {
+  const enableSearch =
+    onSearchChange && (!!searchValue || options.length > SEARCH_INPUT_THRESHOLD || !isEmpty);
+
+  const dropdownClasses = cx('Filter-target', className);
+
+  const handleClear = (event: SyntheticEvent) => {
+    event.preventDefault();
+    onClear?.();
+  };
+
   return (
-    <div className={cx('Filter', className)}>
-      <span>{children}</span>
-    </div>
+    <Dropdown targetClassName={dropdownClasses} {...props}>
+      <FilterButton
+        isClearable={isClearable}
+        onClear={handleClear}
+        name={name}
+        hideName={hideName}
+        isSelected={isSelected}
+        onClickFilterButton={onClickFilterButton}
+        testId={testId}
+      >
+        {description}
+      </FilterButton>
+      <FilterMenu
+        options={options}
+        searchValue={searchValue}
+        searchPlaceholder={searchPlaceholder}
+        searchAriaLabel={searchAriaLabel}
+        enableSearch={enableSearch}
+        onSearchChange={onSearchChange}
+        isLoading={isLoading}
+        disabledOptionTooltip={disabledOptionTooltip}
+        menuWidth={menuWidth}
+        enableVirtualization={enableVirtualization}
+      />
+    </Dropdown>
   );
 };
 
