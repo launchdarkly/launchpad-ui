bb5bd8f7a529e01ad0a4bcc67f7e6b1e56bfbe59
Valerie Roske <vroske@launchdarkly.com>
2022-07-27 17:03:15 -0700

    feat: add Filter component to filter package

--- packages/filter/src/index.ts
+++ packages/filter/src/index.ts
@@ -1,4 +1,5 @@
 export type { FilterProps } from './Filter';
 export type { FilterButtonProps } from './FilterButton';
+export type { FilterOption } from './FilterMenu';
 export { Filter } from './Filter';
 export { FilterButton } from './FilterButton';
