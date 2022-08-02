0de2d16ab5b46d610e389d29bb32501559377612
Robert Niznik <rniznik@launchdarkly.com>
2022-08-02 11:28:38 -0400

    test: ignore coverage for deprecated component

--- packages/menu/src/Menu.tsx
+++ packages/menu/src/Menu.tsx
@@ -116,6 +116,7 @@ const Menu = <T extends number | string>(props: MenuProps<T>) => {
             };
           case MenuItem:
           case MenuItemLink:
+            /* c8 ignore start */
             return {
               items: items.concat(
                 child.props.disabled
@@ -141,6 +142,7 @@ const Menu = <T extends number | string>(props: MenuProps<T>) => {
               ),
               searchElement,
             };
+          /* c8 ignore stop */
           case MenuDivider:
             return { items: items.concat(child), searchElement };
           default:
@@ -271,6 +273,7 @@ const ItemVirtualizer = <T extends number | string>(props: ItemVirtualizerProps<
       switch (itemElem.type) {
         case MenuItem:
         case MenuItemLink:
+          /* c8 ignore start */
           return {
             className: cx(childProps.className, menuItemClassName),
             // set focus on the first menu item if there is no search input, and set in the tab order
@@ -294,6 +297,7 @@ const ItemVirtualizer = <T extends number | string>(props: ItemVirtualizerProps<
                   onSelect?.(childProps.item as T);
                 }),
           } as MenuItemProps<T>;
+        /* c8 ignore stop */
         default:
           return {};
       }
