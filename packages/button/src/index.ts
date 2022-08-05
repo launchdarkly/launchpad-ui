9ea9b63f1db11ce782d9a1e3848ec8d22c7be634
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-08-05 13:52:49 -0500

    feat(button): add ButtonGroup component (#254)
    * feat(button): add ButtonGroup component
    
    * feat(button): add ButtonGroupProps to export list, add stylesheet to core
    
    * feat(button): modify ButtonGroup component tests

--- packages/button/src/index.ts
+++ packages/button/src/index.ts
@@ -1,4 +1,6 @@
 export type { ButtonProps } from './Button';
+export type { ButtonGroupProps } from './ButtonGroup';
 
 export { Button } from './Button';
+export { ButtonGroup } from './ButtonGroup';
 export { ButtonGroupSpacing, ButtonKind, ButtonSize, ButtonType } from './types';
