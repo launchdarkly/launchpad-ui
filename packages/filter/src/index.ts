42d07595aca29160308c04a5d8c05e86c088fe5d
Robert Niznik <rniznik@launchdarkly.com>
2022-08-17 09:03:39 -0400

    fix(filter): export AppliedFilterButton and FilterMenu

--- packages/filter/src/index.ts
+++ packages/filter/src/index.ts
@@ -3,5 +3,7 @@ export type { FilterProps } from './Filter';
 export type { FilterButtonProps } from './FilterButton';
 export type { FilterOption } from './FilterMenu';
 export { AppliedFilter } from './AppliedFilter';
+export { AppliedFilterButton } from './AppliedFilterButton';
 export { Filter } from './Filter';
 export { FilterButton } from './FilterButton';
+export { FilterMenu } from './FilterMenu';
