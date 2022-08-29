77fa702f610b672988e3b8858be7ab13fc9f9f1b
Robert Niznik <rniznik@launchdarkly.com>
2022-08-29 11:31:08 -0400

    fix: address React TS issue

--- packages/icons/src/Icon.tsx
+++ packages/icons/src/Icon.tsx
@@ -1,7 +1,7 @@
 import type { IconSize } from './types';
 
 import { cx } from 'classix';
-import { useEffect, useId, useRef } from 'react';
+import { Children, cloneElement, isValidElement, useEffect, useId, useRef } from 'react';
 
 import './styles/Icon.css';
 
@@ -15,29 +15,35 @@ const Icon = ({ name, subtle, className, size, children, ...props }: IconProps) 
   const sizeClass = size ? `Icon--${size}` : false;
   const classes = cx('Icon', `Icon--${name}`, sizeClass, className, subtle && 'Icon--subtle');
 
-  const ref = useRef<HTMLSpanElement>(null);
+  const svgRef = useRef<SVGElement>(null);
   const prefix = `svg-${useId()}`;
 
   useEffect(() => {
-    const title = ref.current?.querySelector('title');
-    const desc = ref.current?.querySelector('desc');
-    const svg = ref.current?.querySelector('svg');
+    const title = svgRef.current?.querySelector('title');
+    const desc = svgRef.current?.querySelector('desc');
 
     if (title) {
       const id = `${prefix}-${name}-title`;
       title.setAttribute('id', id);
-      svg?.setAttribute('aria-labelledby', id);
+      svgRef.current?.setAttribute('aria-labelledby', id);
     }
     if (desc) {
       const id = `${prefix}-${name}-description`;
       desc.setAttribute('id', id);
-      svg?.setAttribute('aria-describedby', id);
+      svgRef.current?.setAttribute('aria-describedby', id);
     }
   }, [name, prefix]);
 
   return (
-    <span {...props} className={classes} ref={ref}>
-      {children}
+    <span {...props} className={classes}>
+      {Children.map(children, (child) => {
+        if (isValidElement(child)) {
+          return cloneElement(child as React.ReactElement, {
+            ref: svgRef,
+          });
+        }
+        return null;
+      })}
     </span>
   );
 };
