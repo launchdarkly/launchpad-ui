f2d7d049306f58458de66fc1cf36f63eab82c373
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-09-06 23:58:25 -0500

    refactor(button): support passing generic HTML attributes

---
+++ .changeset/brown-guests-thank.md
@@ -0,0 +1,6 @@
+---
+'@launchpad-ui/button': patch
+'@launchpad-ui/core': patch
+---
+
+[Button] Improve prop object type for ButtonGroup and IconButton components to pass through HTML attributes
--- packages/button/src/ButtonGroup.tsx
+++ packages/button/src/ButtonGroup.tsx
@@ -1,28 +1,24 @@
-import type { ReactNode } from 'react';
+import type { HTMLAttributes } from 'react';
 
 import { cx } from 'classix';
 
 import './styles/ButtonGroup.css';
 import { ButtonGroupSpacing } from './types';
 
-type ButtonGroupProps = {
+type ButtonGroupProps = HTMLAttributes<HTMLElement> & {
   spacing?: ButtonGroupSpacing;
-  className?: string;
-  testId?: string;
-  children: ReactNode;
 };
 
 const ButtonGroup = ({
   spacing = ButtonGroupSpacing.NORMAL,
   className,
-  testId,
   children,
   ...rest
 }: ButtonGroupProps) => {
   const classes = cx('ButtonGroup', `ButtonGroup--${spacing}`, className);
 
   return (
-    <div className={classes} data-test-id={testId} {...rest}>
+    <div className={classes} {...rest}>
       {children}
     </div>
   );
