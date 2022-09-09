13fdfa3846d8651c27211330f73032ad8223042f
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-09-08 19:41:06 -0500

    refactor(split-button): enhance html attribute passthrough

--- packages/split-button/src/SplitButton.tsx
+++ packages/split-button/src/SplitButton.tsx
@@ -1,24 +1,21 @@
 import type { ButtonKind, ButtonSize } from '@launchpad-ui/button';
-import type { ReactNode } from 'react';
+import type { HTMLAttributes } from 'react';
 
 import { cx } from 'classix';
 
 import { SplitButtonContext } from './context';
 import './styles/SplitButton.css';
 
-type SplitButtonProps = {
-  className?: string;
+type SplitButtonProps = HTMLAttributes<HTMLDivElement> & {
   kind?: ButtonKind.PRIMARY | ButtonKind.DEFAULT;
   size?: ButtonSize;
   disabled?: boolean;
-  testId?: string;
-  children?: ReactNode;
 };
 
-const SplitButton = ({ disabled, kind, size, children, testId, className }: SplitButtonProps) => {
+const SplitButton = ({ disabled, kind, size, children, className, ...rest }: SplitButtonProps) => {
   return (
     <SplitButtonContext.Provider value={{ disabled: !!disabled, kind, size }}>
-      <div className={cx('SplitButton', className)} data-test-id={testId}>
+      <div {...rest} className={cx('SplitButton', className)}>
         {children}
       </div>
     </SplitButtonContext.Provider>
