5ddaa463da769dd7ad90d6a07e8ab5f424fa7df0
Robert Niznik <rniznik@launchdarkly.com>
2022-09-14 14:17:18 -0400

    fix(snackbar,toast): export record types (#374)
    * fix(snackbar,toast): export record types
    
    * chore: add changeset

--- packages/snackbar/src/index.ts
+++ packages/snackbar/src/index.ts
@@ -1,5 +1,6 @@
 export type { SnackbarProps } from './Snackbar';
 export type { SnackbarCenterProps } from './SnackbarCenter';
+export type { SnackbarRecord } from './types';
 export { Snackbar } from './Snackbar';
 export { SnackbarCenter } from './SnackbarCenter';
 export { SnackbarKind } from './types';
