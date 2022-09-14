bc7cf497ec50e7a21160d386d606c48938a1f455
Robert Niznik <rniznik@launchdarkly.com>
2022-09-14 14:15:04 -0400

    fix(snackbar,toast): export record types

--- packages/toast/src/index.ts
+++ packages/toast/src/index.ts
@@ -1,5 +1,6 @@
 export type { ToastProps } from './Toast';
 export type { ToastCenterProps } from './ToastCenter';
+export type { ToastRecord } from './types';
 export { Toast } from './Toast';
 export { ToastCenter } from './ToastCenter';
 export { ToastKind } from './types';
