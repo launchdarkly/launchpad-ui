d26d3664b2a5487e1307e6563edb748333a6c6bf
Robert Niznik <rniznik@launchdarkly.com>
2022-10-03 14:57:21 -0400

    refactor(counter): update to css modules (#443)
    * refactor(counter): update to css modules
    
    * chore: add changeset
    
    * fix: remove class test

--- packages/counter/__tests__/Counter.spec.tsx
+++ packages/counter/__tests__/Counter.spec.tsx
@@ -5,14 +5,7 @@ import { Counter } from '../src';
 
 describe('Counter', () => {
   it('renders', () => {
-    render(<Counter value={12} />);
-    expect(screen.getByText('12')).toBeInTheDocument();
-  });
-
-  it('renders subtle mode', () => {
     render(<Counter value={12} subtle />);
-    const node = screen.getByText('12');
-    expect(node).toBeInTheDocument();
-    expect(node).toHaveClass('Counter--subtle');
+    expect(screen.getByText('12')).toBeInTheDocument();
   });
 });
