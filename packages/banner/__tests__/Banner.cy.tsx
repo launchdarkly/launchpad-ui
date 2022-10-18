b34c38550ed10a69d4c7de3d8044b894a04d45cb
Robert Niznik <rniznik@launchdarkly.com>
2022-10-18 14:32:29 -0400

    test(banner): update cy tests

--- packages/banner/__tests__/Banner.cy.tsx
+++ packages/banner/__tests__/Banner.cy.tsx
@@ -10,4 +10,38 @@ describe('Banner', () => {
     cy.mount(<Banner kind="info">Banner</Banner>);
     cy.checkA11y();
   });
+
+  it('can be dismissible', () => {
+    cy.mount(
+      <Banner kind="info" dismissible>
+        An important message
+      </Banner>
+    );
+    cy.get('button').should('be.visible');
+  });
+
+  it('renders a header when passed', () => {
+    const header = 'My header';
+    cy.mount(
+      <Banner kind="info" header={header}>
+        An important message
+      </Banner>
+    );
+
+    cy.contains(header).should('be.visible');
+  });
+
+  it('triggers onDismiss when close button is clicked', () => {
+    const onDismissSpy = cy.spy().as('onDismissSpy');
+    const content = 'An important message';
+    cy.mount(
+      <Banner kind="info" onDismiss={onDismissSpy} dismissible>
+        {content}
+      </Banner>
+    );
+
+    cy.get('button').click();
+
+    cy.get('@onDismissSpy').should('have.been.calledOnce');
+  });
 });
