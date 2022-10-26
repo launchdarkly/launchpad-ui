e4fe60dd9cdd5885b66589f6ae23fbe97fa5ae03
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-10-26 15:57:04 -0500

    refactor(modal): update tests

--- packages/modal/__tests__/Modal.cy.tsx
+++ packages/modal/__tests__/Modal.cy.tsx
@@ -1,53 +1,36 @@
-import { Modal, ModalBody, ModalFooter, ModalHeader, ModalSheet, Prompt } from '../src';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from '../src';
 
 describe('Modal', () => {
   it('renders', () => {
     cy.mount(
-      <Prompt>
-        <Modal transition="pop" withCloseButton>
-          <ModalHeader>Modal</ModalHeader>
-          <ModalBody>Body</ModalBody>
-          <ModalFooter>Footer</ModalFooter>
-        </Modal>
-      </Prompt>
+      <Modal>
+        <ModalHeader>Modal</ModalHeader>
+        <ModalBody>Body</ModalBody>
+        <ModalFooter>Footer</ModalFooter>
+      </Modal>
     );
     cy.getByTestId('modal').should('be.visible');
   });
 
   it('is accessible', () => {
     cy.mount(
-      <Prompt>
-        <Modal transition="pop" withCloseButton>
-          <ModalHeader>Modal</ModalHeader>
-          <ModalBody>Body</ModalBody>
-          <ModalFooter>Footer</ModalFooter>
-        </Modal>
-      </Prompt>
+      <Modal withCloseButton>
+        <ModalHeader>Modal</ModalHeader>
+        <ModalBody>Body</ModalBody>
+        <ModalFooter>Footer</ModalFooter>
+      </Modal>
     );
     cy.checkA11y();
-  });
-
-  it('can render as a sheet', () => {
-    cy.mount(
-      <ModalSheet withCloseButton>
-        <section>
-          <ModalHeader closeable>Modal</ModalHeader>
-        </section>
-      </ModalSheet>
-    );
-    cy.get('[role="dialog').should('be.visible');
   });
 
   it('calls onCancel when escape key is pressed', () => {
     const onCancelSpy = cy.spy().as('onCancelSpy');
     cy.mount(
-      <Prompt>
-        <Modal transition="pop" onCancel={onCancelSpy}>
-          <ModalHeader>Modal</ModalHeader>
-          <ModalBody>Body</ModalBody>
-          <ModalFooter>Footer</ModalFooter>
-        </Modal>
-      </Prompt>
+      <Modal onCancel={onCancelSpy}>
+        <ModalHeader>Modal</ModalHeader>
+        <ModalBody>Body</ModalBody>
+        <ModalFooter>Footer</ModalFooter>
+      </Modal>
     );
 
     cy.getByTestId('modal').type('{esc}');
