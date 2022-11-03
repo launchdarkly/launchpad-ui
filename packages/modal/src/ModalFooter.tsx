1e6fd8f5e0bab976b0bd0f19ed4dbfe5586b8cdf
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-11-03 12:18:33 -0500

    refactor(modal): Add `AbsoluteModalFooter` component (#546)
    * refactor(modal): position footer absolutely
    
    * refactor(modal): add absolutemodalfooter

--- packages/modal/src/ModalFooter.tsx
+++ packages/modal/src/ModalFooter.tsx
@@ -1,19 +1,25 @@
 import type { ModalProps } from './Modal';
 
 import { ButtonGroup } from '@launchpad-ui/button';
+import cx from 'classix';
+import { forwardRef } from 'react';
 
 import styles from './styles/Modal.module.css';
 
-type ModalFooterProps = Pick<ModalProps, 'secondaryButton' | 'primaryButton'>;
+type ModalFooterProps = Pick<ModalProps, 'secondaryButton' | 'primaryButton' | 'className'>;
 
-const ModalFooter = ({ secondaryButton, primaryButton }: ModalFooterProps) => (
-  <div className={styles.footer} data-test-id="modal-footer">
-    <ButtonGroup className={styles.footerActions}>
-      {secondaryButton}
-      {primaryButton}
-    </ButtonGroup>
-  </div>
+const ModalFooter = forwardRef<HTMLDivElement, ModalFooterProps>(
+  ({ secondaryButton, primaryButton, className }, ref) => (
+    <div className={cx(className, styles.footer)} data-test-id="modal-footer" ref={ref}>
+      <ButtonGroup className={styles.footerActions}>
+        {secondaryButton}
+        {primaryButton}
+      </ButtonGroup>
+    </div>
+  )
 );
+
+ModalFooter.displayName = 'ModalFooter';
 
 export { ModalFooter };
 export type { ModalFooterProps };
