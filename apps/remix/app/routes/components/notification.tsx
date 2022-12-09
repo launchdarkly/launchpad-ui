12bdd2bb39ff45ab196ac59eed8d17b718c8877c
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2022-12-09 16:35:37 -0600

    chore(notification): remove package

--- apps/remix/app/routes/components/notification.tsx
+++
@@ -1,7 +0,0 @@
-import { Notification } from '@launchpad-ui/core';
-
-export default function Index() {
-  return (
-    <Notification level="success" message="This is a notification" onDismiss={() => undefined} />
-  );
-}
