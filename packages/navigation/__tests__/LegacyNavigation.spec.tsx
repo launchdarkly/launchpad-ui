91b99873b8a45dde75e37c34f27256b4dddd395c
Keegan Watkins <kwatkins@launchdarkly.com>
2023-01-04 15:15:54 -0600

    feat: add legacy Navigation components

---
+++ packages/navigation/__tests__/LegacyNavigation.spec.tsx
@@ -0,0 +1,119 @@
+import type { LegacyNavigationItemProps } from '../src';
+
+import { MemoryRouter } from 'react-router-dom';
+import { it, expect, describe, vi } from 'vitest';
+
+import { render, screen, userEvent, waitFor } from '../../../test/utils';
+import { LegacyNavigation, LegacyNavigationItem } from '../src';
+import * as ctx from '../src/legacy/NavigationContext';
+
+globalThis.matchMedia = vi.fn().mockReturnValue({
+  matches: true,
+  onchange: null,
+  addEventListener: vi.fn(),
+  removeEventListener: vi.fn(),
+  dispatchEvent: vi.fn(),
+  addListener: vi.fn(),
+  removeListener: vi.fn(),
+});
+
+const createComponent = (items: LegacyNavigationItemProps[]) => (
+  <MemoryRouter>
+    <LegacyNavigation items={items} title="nav" kind="primary">
+      {(item) => (
+        <LegacyNavigationItem
+          key={item.to}
+          name={item.name}
+          to={item.to}
+          tooltip={item.tooltip}
+          tooltipContent={item.tooltipContent}
+          status={item.status}
+        />
+      )}
+    </LegacyNavigation>
+  </MemoryRouter>
+);
+
+describe('Navigation', () => {
+  it('renders', () => {
+    render(
+      createComponent([
+        {
+          name: 'First',
+          to: '/first',
+        },
+        {
+          name: 'Second',
+          to: '/second',
+        },
+      ])
+    );
+    expect(screen.getByRole('navigation')).toBeInTheDocument();
+  });
+
+  it('can render items with a tooltip', async () => {
+    const user = userEvent.setup();
+    render(
+      createComponent([
+        {
+          name: 'First',
+          to: '/first',
+          tooltip: true,
+          tooltipContent: 'one',
+        },
+        {
+          name: 'Second',
+          to: '/second',
+        },
+      ])
+    );
+
+    await user.hover(screen.getByText('First'));
+    await waitFor(() => {
+      expect(screen.getByRole('tooltip')).toBeInTheDocument();
+    });
+  });
+
+  it('can render items with a chip', async () => {
+    render(
+      createComponent([
+        {
+          name: 'First',
+          to: '/first',
+          status: 'new',
+        },
+        {
+          name: 'Second',
+          to: '/second',
+          tooltip: <>tooltip</>,
+        },
+      ])
+    );
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    expect(screen.getByTestId('nav-item-chip')).not.toBeNull();
+  });
+
+  it('renders collapsed dropdown', async () => {
+    vi.spyOn(ctx, 'useNavigationContext').mockReturnValue({
+      shouldCollapse: true,
+      refs: { wrapperRef: { current: null }, itemListRef: { current: null } },
+    });
+
+    render(
+      createComponent([
+        {
+          name: 'First',
+          to: '/first',
+          status: 'new',
+        },
+        {
+          name: 'Second',
+          to: '/second',
+          tooltip: <>tooltip</>,
+        },
+      ])
+    );
+    // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    expect(screen.getByTestId('navigation-menu-button')).not.toBeNull();
+  });
+});
---
+++ packages/navigation/src/legacy/NavItem.tsx
@@ -0,0 +1,64 @@
+import type { ChipProps } from '@launchpad-ui/chip';
+import type { MouseEvent } from 'react';
+
+import { Chip } from '@launchpad-ui/chip';
+import { cx } from 'classix';
+import { NavLink, useLocation } from 'react-router-dom';
+
+import styles from './styles/Navigation.module.css';
+import { titlecase } from './utils';
+
+type NavItemProps = {
+  to: string | { pathname: string; search: string };
+  name: string;
+  end?: boolean;
+  onClick?(event: MouseEvent): void;
+  activeClassName?: string;
+  status?: ChipProps['kind'];
+  id?: string;
+  role?: string;
+  'data-test-id'?: string;
+};
+
+const NavItem = ({
+  to,
+  name,
+  onClick,
+  status,
+  role,
+  end,
+  'data-test-id': testId = 'nav-item',
+  ...other
+}: NavItemProps) => {
+  const { pathname } = useLocation();
+  const selected = pathname === to ? 'true' : 'false';
+
+  return (
+    <NavLink
+      {...other}
+      end={end}
+      to={to}
+      className={({ isActive }) => cx(styles.NavItem, isActive && styles['is-active'])}
+      data-text={name}
+      onClick={onClick}
+      role={role}
+      data-nav-target="true" // used by Navigation to check rendered width
+      data-test-id={testId}
+      aria-selected={role === 'tab' ? selected : undefined}
+    >
+      {status ? (
+        <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+          <span className={styles['NavItem-name']}>{name}</span>
+          <Chip className={styles['NavItem-chip']} data-test-id="nav-item-chip" kind={status}>
+            {titlecase(status)}
+          </Chip>
+        </div>
+      ) : (
+        <span className={styles['NavItem-name']}>{name}</span>
+      )}
+    </NavLink>
+  );
+};
+
+export { NavItem };
+export type { NavItemProps };
