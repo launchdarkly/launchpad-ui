91b5405c9b3c0eab6d203384b0f29e18e463467f
Keegan Watkins <109103953+kwatkins-ld@users.noreply.github.com>
2023-02-06 17:21:29 -0600

    chore: remove legacy Navigation components (#651)

--- packages/navigation/src/index.ts
+++ packages/navigation/src/index.ts
@@ -4,10 +4,3 @@ export type { NavigationItemProps } from './NavigationItem';
 export { Navigation } from './Navigation';
 export { NavigationItem } from './NavigationItem';
 /* c8 ignore stop */
-
-export type { NavigationProps as LegacyNavigationProps } from './legacy/Navigation';
-export type { NavigationItemProps as LegacyNavigationItemProps } from './legacy/NavigationItem';
-/* c8 ignore start */
-export { Navigation as LegacyNavigation } from './legacy/Navigation';
-export { NavigationItem as LegacyNavigationItem } from './legacy/NavigationItem';
-/* c8 ignore stop */
