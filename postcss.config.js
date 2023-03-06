ddf1d8b070e6bfb07b1804ad933351c529091d22
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-06 14:46:58 -0600

    feat: support nested themes

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-nested'),
-    require('postcss-preset-env'),
-    require('postcss-custom-selectors'),
-  ],
+  plugins: [require('postcss-import'), require('postcss-nested'), require('postcss-preset-env')],
 };
