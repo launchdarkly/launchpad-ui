08ca611c65ec6d1dd1b25d6bb389c1fc3049bad2
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-07 17:14:28 -0600

    feat(chip): update default styles

--- packages/chip/stories/Chip.stories.tsx
+++ packages/chip/stories/Chip.stories.tsx
@@ -22,11 +22,6 @@ export default {
         category: 'Presentation',
       },
     },
-    subtle: {
-      table: {
-        category: 'Presentation',
-      },
-    },
     children: {
       table: {
         category: 'Content',
@@ -47,8 +42,6 @@ export const Warning: Story = { args: { children: 'Warning Chip', kind: 'warning
 
 export const Info: Story = { args: { children: 'Info Chip', kind: 'info' } };
 
-export const Label: Story = { args: { children: 'Label Chip', kind: 'label' } };
-
 export const New: Story = { args: { children: 'New Chip', kind: 'new' } };
 
 export const Federal: Story = { args: { children: 'Federal Chip', kind: 'federal' } };
@@ -56,37 +49,3 @@ export const Federal: Story = { args: { children: 'Federal Chip', kind: 'federal
 export const Beta: Story = { args: { children: 'Beta Chip', kind: 'beta' } };
 
 export const Small: Story = { args: { children: 'Small Chip', size: 'small', kind: 'success' } };
-
-export const Medium: Story = { args: { children: 'Medium Chip', size: 'medium', kind: 'success' } };
-
-export const Interactive: Story = {
-  args: {
-    children: 'Interactive Chip',
-    onClick: () => {
-      alert('Chip clicked');
-    },
-  },
-};
-
-export const DefaultSubtle: Story = {
-  args: { children: 'Example Chip', subtle: true },
-};
-
-export const SuccessSubtle: Story = {
-  args: { children: 'Success Chip', kind: 'success', subtle: true },
-};
-
-export const WarningSubtle: Story = {
-  args: { children: 'Warning Chip', kind: 'warning', subtle: true },
-};
-export const InfoSubtle: Story = { args: { children: 'Info Chip', kind: 'info', subtle: true } };
-
-export const LabelSubtle: Story = { args: { children: 'Label Chip', kind: 'label', subtle: true } };
-
-export const NewSubtle: Story = { args: { children: 'New Chip', kind: 'new', subtle: true } };
-
-export const FederalSubtle: Story = {
-  args: { children: 'Federal Chip', kind: 'federal', subtle: true },
-};
-
-export const BetaSubtle: Story = { args: { children: 'Beta Chip', kind: 'beta', subtle: true } };
