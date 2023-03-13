90239c5e5fdabb7a0b00cc493d40592b5cadf6d6
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-13 10:36:35 -0500

    tags

--- packages/tag/src/Tag.tsx
+++ packages/tag/src/Tag.tsx
@@ -1,20 +1,61 @@
-import type { HTMLAttributes } from 'react';
-
-import { cx } from 'classix';
+import cx from 'classix';
+import { mergeProps } from '@react-aria/utils';
+import React, { useRef } from 'react';
+import type { TagGroupState } from '@react-stately/tag';
+import { TagProps } from '@react-types/tag';
+import { useFocusRing } from '@react-aria/focus';
+import { useHover } from '@react-aria/interactions';
+import { useTag } from '@react-aria/tag';
 
 import styles from './styles/Tag.module.css';
+import { IconButton } from '@launchpad-ui/button';
+import { Close } from '@launchpad-ui/icons';
 
-type TagProps = HTMLAttributes<HTMLDivElement> & {
-  'data-test-id'?: string;
-};
+export interface SpectrumTagProps<T> extends TagProps<T> {
+  state: TagGroupState<T>;
+}
 
-const Tag = ({ children, className, 'data-test-id': testId = 'tag' }: TagProps) => {
+const Tag = <T extends object>(props: SpectrumTagProps<T>) => {
+  const { children, allowsRemoving, item, state, onRemove, ...otherProps } = props;
+
+  const { hoverProps, isHovered } = useHover({});
+  const { isFocused, isFocusVisible, focusProps } = useFocusRing({ within: true });
+  const ref = useRef();
+  const { clearButtonProps, labelProps, gridCellProps, rowProps } = useTag(
+    {
+      ...props,
+      isFocused,
+      allowsRemoving,
+      item,
+      onRemove,
+    },
+    state,
+    ref
+  );
+
   return (
-    <div className={cx(styles.tag, className)} data-test-id={testId}>
-      <span>{children}</span>
+    <div
+      {...mergeProps(rowProps, hoverProps, focusProps)}
+      className={cx(styles.tag, {
+        'focus-ring': isFocusVisible,
+        'is-focused': isFocused,
+        'is-hovered': isHovered,
+        'is-removable': allowsRemoving,
+      })}
+      ref={ref}
+    >
+      <div className={cx(styles.tagCell)} {...gridCellProps}>
+        {typeof children === 'string' ? <span>{children}</span> : children}
+        {allowsRemoving && (
+          <IconButton
+            icon={<Close />}
+            kind="minimal"
+            size="small"
+            className={styles.removeButton}
+            {...props}
+          />
+        )}
+      </div>
     </div>
   );
 };
-
-export { Tag };
-export type { TagProps };
