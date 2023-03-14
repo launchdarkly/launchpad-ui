2551d6a5728d1d48592069d9c2d7e9bf139257b4
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-14 12:19:43 -0500

    feat(tag): add add remix route

--- apps/remix/app/routes/components/tag.tsx
+++ apps/remix/app/routes/components/tag.tsx
@@ -1,7 +1,16 @@
-import { Tag } from '@launchpad-ui/core';
+import { TagGroup, TagItem } from '@launchpad-ui/core';
+
+const MOCK_TAGS = [
+  { id: 1, name: 'News' },
+  { id: 2, name: 'Travel' },
+  { id: 3, name: 'Gaming' },
+  { id: 4, name: 'Shopping' },
+  { id: 5, name: 'Sports' },
+  { id: 6, name: 'Music' },
+  { id: 7, name: 'Documentaries' },
+  { id: 8, name: 'History' },
+];
 
 export default function Index() {
-  return (
-    <Tag>A lovely Tag component.</Tag>
-  );
+  return <TagGroup items={MOCK_TAGS}>{(item) => <TagItem>{item.name}</TagItem>}</TagGroup>;
 }
