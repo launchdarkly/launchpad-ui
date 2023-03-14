b0683084e491eff810738c658c3d5f900865087b
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-14 12:28:25 -0500

    feat(tag): fix cypress test

--- packages/tag/__tests__/Tag.cy.tsx
+++ packages/tag/__tests__/Tag.cy.tsx
@@ -1,13 +1,15 @@
-import { Tag } from '../src';
+import { TagGroup, TagItem } from '../src';
+
+import { MOCK_TAGS } from './constants';
 
 describe('Tag', () => {
   it('renders', () => {
-    cy.mount(<Tag>An important message</Tag>);
+    cy.mount(<TagGroup items={MOCK_TAGS}>{(item) => <TagItem>{item.name}</TagItem>}</TagGroup>);
     cy.getByTestId('tag').should('be.visible');
   });
 
   it('is accessible', () => {
-    cy.mount(<Tag>An important message</Tag>);
+    cy.mount(<TagGroup items={MOCK_TAGS}>{(item) => <TagItem>{item.name}</TagItem>}</TagGroup>);
     cy.checkA11y();
   });
 });
