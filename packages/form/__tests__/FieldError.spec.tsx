479083181569652d3393f91b8b54a2a9fe2d9419
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-16 16:38:29 -0500

    feat(select): integrate select with tags

--- packages/form/__tests__/FieldError.spec.tsx
+++ packages/form/__tests__/FieldError.spec.tsx
@@ -6,11 +6,11 @@ import { FieldError } from '../src';
 describe('FieldError', () => {
   it('renders', () => {
     render(<FieldError name="Email" errorMessage="Email not found" />);
-    expect(screen.getByText('Error - Email not found')).toBeInTheDocument();
+    expect(screen.getByText('Email not found')).toBeInTheDocument();
   });
 
   it('does not render if no errorMessage is passed', () => {
     render(<FieldError name="Email" />);
-    expect(screen.queryByText('Error - Email not found')).not.toBeInTheDocument();
+    expect(screen.queryByText('Email not found')).not.toBeInTheDocument();
   });
 });
