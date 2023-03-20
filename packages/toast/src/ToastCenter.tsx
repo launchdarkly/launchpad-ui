89055cdee460ff9151dfb33a375f1ee5ce6a2fe1
Jesus Garnica <gtechy100@gmail.com>
2023-03-20 15:27:59 -0700

    feat(toast): speed up exit animation slightly

--- packages/toast/src/ToastCenter.tsx
+++ packages/toast/src/ToastCenter.tsx
@@ -53,7 +53,7 @@ const ToastCenter = ({ toasts, onDismiss, className }: ToastCenterProps) => {
                 opacity: 0,
                 scale: 0.85,
                 transition: {
-                  duration: 0.2,
+                  duration: 0.15,
                   ease: [0.4, 0, 1, 1],
                 },
               }}
