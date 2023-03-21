cad5566a0904330a258f7848fabf16301d347a85
Chase Wackerfuss <104940219+chasedarkly@users.noreply.github.com>
2023-03-21 10:07:51 -0500

    feat(select): style updates

--- packages/select/src/TagSelect/index.ts
+++
@@ -1,6 +0,0 @@
-export type { TagSelectProps } from './TagSelect';
-export type { TagSelectState } from './useTagSelectState';
-export type { TagSelectTriggerProps, TagSelectTriggerChildrenState } from './TagSelectTrigger';
-
-export { TagSelect } from './TagSelect';
-export { TagSelectTrigger } from './TagSelectTrigger';
