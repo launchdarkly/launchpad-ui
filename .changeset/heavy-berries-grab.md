ac2dea48df7325bf6d47b3587be310ea35c33143
Christie Williams <xie.williams@gmail.com>
2023-06-02 15:34:22 -0400

    fix(form): selectField ref forwarding (#856)
    * fix: selectField ref forwarding
    
    * chore: add changeset

---
+++ .changeset/heavy-berries-grab.md
@@ -0,0 +1,6 @@
+---
+'@launchpad-ui/form': patch
+'@launchpad-ui/core': patch
+---
+
+Ref forwarding for SelectField
--- packages/form/src/SelectField.tsx
+++ packages/form/src/SelectField.tsx
@@ -1,6 +1,7 @@
 import type { ComponentProps } from 'react';
 
 import { cx } from 'classix';
+import { forwardRef } from 'react';
 
 import styles from './styles/Form.module.css';
 
@@ -8,20 +9,19 @@ type SelectFieldProps = ComponentProps<'select'> & {
   'data-test-id'?: string;
 };
 
-const SelectField = ({
-  className,
-  children,
-  'data-test-id': testId = 'select',
-  ...rest
-}: SelectFieldProps) => {
-  const classes = cx(styles.formInput, className);
+const SelectField = forwardRef<HTMLSelectElement, SelectFieldProps>(
+  ({ className, children, 'data-test-id': testId = 'select', ...rest }: SelectFieldProps, ref) => {
+    const classes = cx(styles.formInput, className);
 
-  return (
-    <select {...rest} data-test-id={testId} className={classes}>
-      {children}
-    </select>
-  );
-};
+    return (
+      <select {...rest} data-test-id={testId} className={classes} ref={ref}>
+        {children}
+      </select>
+    );
+  }
+);
+
+SelectField.displayName = 'SelectField';
 
 export { SelectField };
 export type { SelectFieldProps };
