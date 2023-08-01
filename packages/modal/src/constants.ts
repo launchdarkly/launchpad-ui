057e9eeae2d6878363f493820e6b015828dda12d
Christina Chen <cchen@launchdarkly.com>
2023-08-01 08:04:59 -0500

    fix(modal): use modal description as aria-describedby (#942)
    * fix(modal): use modal description as aria-described-by
    
    * fix(modal): add changelog

--- packages/modal/src/constants.ts
+++ packages/modal/src/constants.ts
@@ -1,1 +1,4 @@
-export const MODAL_LABELLED_BY = 'modal-title';
+const MODAL_LABELLED_BY = 'modal-title';
+const MODAL_DESCRIBED_BY = 'modal-description';
+
+export { MODAL_DESCRIBED_BY, MODAL_LABELLED_BY };
