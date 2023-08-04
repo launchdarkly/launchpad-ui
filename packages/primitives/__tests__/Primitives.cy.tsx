66c96ad22c2ce1be546bc0c7a49493500956d6a9
Robb Niznik <rniznik@launchdarkly.com>
2023-08-04 15:25:49 -0400

    feat: add tests

--- packages/primitives/__tests__/Primitives.cy.tsx
+++ packages/primitives/__tests__/Primitives.cy.tsx
@@ -1,18 +1,20 @@
 import type { PopoverProps } from '../src';
 
+import { Item, Section } from '@react-stately/collections';
 import { useOverlayTriggerState } from '@react-stately/overlays';
 import { useRef } from 'react';
 
-import { Popover } from '../src';
+import { Popover, Button, ListBox } from '../src';
 
 const PopoverPrimitive = ({ ...props }: Partial<PopoverProps>) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
   const triggerRef = useRef<HTMLButtonElement>(null);
   const state = useOverlayTriggerState({});
 
   return (
     <>
       <button ref={triggerRef}>trigger</button>
-      <Popover state={state} triggerRef={triggerRef} {...props}>
+      <Popover state={state} popoverRef={popoverRef} triggerRef={triggerRef} {...props}>
         An important message
       </Popover>
     </>
@@ -28,6 +30,42 @@ describe('Primitives', () => {
 
     it('is accessible', () => {
       cy.mount(<PopoverPrimitive />);
+      cy.checkA11y();
+    });
+  });
+
+  describe('ListBox', () => {
+    it('renders', () => {
+      cy.mount(
+        <ListBox aria-label="listbox">
+          <Item key="one">One</Item>
+        </ListBox>
+      );
+      cy.getByRole('listbox').should('be.visible');
+    });
+
+    it('is accessible', () => {
+      cy.mount(
+        <ListBox aria-label="listbox">
+          <Section title="section">
+            <Item key="one">One</Item>
+            <Item key="two">Two</Item>
+            <Item key="three">Three</Item>
+          </Section>
+        </ListBox>
+      );
+      cy.checkA11y();
+    });
+  });
+
+  describe('Button', () => {
+    it('renders', () => {
+      cy.mount(<Button>Hi</Button>);
+      cy.getByRole('button').should('be.visible');
+    });
+
+    it('is accessible', () => {
+      cy.mount(<Button>Hi</Button>);
       cy.checkA11y();
     });
   });
--- packages/primitives/__tests__/Primitives.spec.tsx
+++ packages/primitives/__tests__/Primitives.spec.tsx
@@ -1,20 +1,22 @@
 import type { PopoverProps } from '../src';
 
+import { Item, Section } from '@react-stately/collections';
 import { useOverlayTriggerState } from '@react-stately/overlays';
 import { useRef } from 'react';
 import { it, expect, describe } from 'vitest';
 
 import { render, screen } from '../../../test/utils';
-import { Popover } from '../src';
+import { Popover, Button, ListBox } from '../src';
 
 const PopoverPrimitive = ({ ...props }: Partial<PopoverProps>) => {
+  const popoverRef = useRef<HTMLDivElement>(null);
   const triggerRef = useRef<HTMLButtonElement>(null);
   const state = useOverlayTriggerState({});
 
   return (
     <>
       <button ref={triggerRef}>trigger</button>
-      <Popover state={state} triggerRef={triggerRef} {...props}>
+      <Popover state={state} popoverRef={popoverRef} triggerRef={triggerRef} {...props}>
         An important message
       </Popover>
     </>
@@ -26,6 +28,28 @@ describe('Primitives', () => {
     it('renders', () => {
       render(<PopoverPrimitive />);
       expect(screen.getByText('An important message')).toBeVisible();
+    });
+  });
+
+  describe('ListBox', () => {
+    it('renders', () => {
+      render(
+        <ListBox aria-label="listbox">
+          <Section title="section">
+            <Item key="one">One</Item>
+            <Item key="two">Two</Item>
+            <Item key="three">Three</Item>
+          </Section>
+        </ListBox>
+      );
+      expect(screen.getByRole('listbox')).toBeVisible();
+    });
+  });
+
+  describe('Button', () => {
+    it('renders', () => {
+      render(<Button>Hi</Button>);
+      expect(screen.getByRole('button')).toBeVisible();
     });
   });
 });
