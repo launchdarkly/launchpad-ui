5920c50de63cc1e74294f6dfc67c222d569c4067
Robb Niznik <rniznik@launchdarkly.com>
2023-08-11 11:30:06 -0400

    feat: add peer dep

--- apps/remix/app/routes/components.data-table.tsx
+++ apps/remix/app/routes/components.data-table.tsx
@@ -1,5 +1,24 @@
 import { DataTable } from '@launchpad-ui/core';
+import { Cell, Column, Row, TableBody, TableHeader } from '@react-stately/table';
+
+const COLUMNS = [
+  { name: 'Col 1', key: 'first' },
+  { name: 'Col 2', key: 'second' },
+  { name: 'Col 3', key: 'third' },
+];
+
+const ROWS = [
+  { first: 'one', second: 'two', third: 'three', key: 1 },
+  { first: 'four', second: 'five', third: 'six', key: 2 },
+];
 
 export default function Index() {
-  return <DataTable>A lovely DataTable component.</DataTable>;
+  return (
+    <DataTable>
+      <TableHeader columns={COLUMNS}>{(column) => <Column>{column.name}</Column>}</TableHeader>
+      <TableBody items={ROWS}>
+        {(item) => <Row>{(columnKey) => <Cell>{item[columnKey as keyof typeof item]}</Cell>}</Row>}
+      </TableBody>
+    </DataTable>
+  );
 }
