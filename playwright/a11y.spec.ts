b178cdfa591dd7d7c8b4f2b44013e8606c728eee
Robb Niznik <rniznik@launchdarkly.com>
2023-08-28 16:18:03 -0400

    feat: include story context

--- playwright/a11y.spec.ts
+++ playwright/a11y.spec.ts
@@ -13,16 +13,40 @@ test.describe('Storybook a11y', async () => {
         `${process.env.STORYBOOK_URL}iframe.html?args=&globals=theme:side-by-side&id=${story}&viewMode=story`
       );
 
+      await page.addScriptTag({
+        content: `
+          async function __getContext(storyId) {
+            return globalThis.__STORYBOOK_PREVIEW__.storyStore.loadStory({ storyId });
+          }
+        `,
+      });
+
+      const storyContext = await page.evaluate(({ storyId }) => globalThis.__getContext(storyId), {
+        storyId: story,
+      });
+
+      // Skip stories that disable a11y
+      if (storyContext.parameters?.a11y?.disable) {
+        return;
+      }
+
       const root = page.locator('#storybook-root');
       await root.waitFor();
 
       const accessibilityScanResults = await new AxeBuilder({ page })
         .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
-        .disableRules('region')
+        .disableRules([
+          'region',
+          'landmark-unique',
+          'landmark-one-main',
+          'page-has-heading-one',
+          'scrollable-region-focusable',
+        ])
         .include('#storybook-root')
+        .include('[data-test-id="portal"]')
         .analyze();
 
-      await expect(accessibilityScanResults.violations).not.toBeNull();
+      await expect(accessibilityScanResults.violations).toEqual([]);
     });
   }
 });
