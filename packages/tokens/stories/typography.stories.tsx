aaaa0b5eab3a87eba6d3a51c0895e58f5f01477b
Robb Niznik <rniznik@launchdarkly.com>
2023-10-12 16:11:08 -0400

    feat(tokens): add typography tokens

---
+++ packages/tokens/stories/typography.stories.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+export default {
+  title: 'Tokens/Typography',
+};
+
+export const Typography = {
+  render: () => {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-900)' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <h1 style={{ font: 'var(--lp-heading-1-medium)' }}>Heading 1 - Medium</h1>
+          <h1 style={{ font: 'var(--lp-heading-1-semibold)' }}>Heading 1 - SemiBold</h1>
+          <h1 style={{ font: 'var(--lp-heading-1-extrabold)' }}>Heading 1 - ExtraBold</h1>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <h2 style={{ font: 'var(--lp-heading-2-medium)' }}>Heading 2 - Medium</h2>
+          <h2 style={{ font: 'var(--lp-heading-2-semibold)' }}>Heading 2 - SemiBold</h2>
+          <h2 style={{ font: 'var(--lp-heading-2-extrabold)' }}>Heading 2 - ExtraBold</h2>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <h3 style={{ font: 'var(--lp-heading-3-medium)' }}>Heading 3 - Medium</h3>
+          <h3 style={{ font: 'var(--lp-heading-3-semibold)' }}>Heading 3 - SemiBold</h3>
+          <h3 style={{ font: 'var(--lp-heading-3-extrabold)' }}>Heading 3 - ExtraBold</h3>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <span style={{ font: 'var(--lp-body-1-regular)' }}>Body 1 - Regular</span>
+          <span style={{ font: 'var(--lp-body-1-semibold)' }}>Body 1 - SemiBold</span>
+          <span style={{ font: 'var(--lp-body-1-extrabold)' }}>Body 1 - ExtraBold</span>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <span style={{ font: 'var(--lp-body-2-regular)' }}>Body 2 - Regular</span>
+          <span style={{ font: 'var(--lp-body-2-semibold)' }}>Body 2 - SemiBold</span>
+          <span style={{ font: 'var(--lp-body-2-extrabold)' }}>Body 2 - ExtraBold</span>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <span style={{ font: 'var(--lp-small-1-regular)' }}>Small 1 - Regular</span>
+          <span style={{ font: 'var(--lp-small-1-medium)' }}>Small 1 - Medium</span>
+          <span style={{ font: 'var(--lp-small-1-extrabold)' }}>Small 1 - ExtraBold</span>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <span style={{ font: 'var(--lp-small-2-regular)' }}>Small 2 - Regular</span>
+          <span style={{ font: 'var(--lp-small-2-medium)' }}>Small 2 - Medium</span>
+          <span style={{ font: 'var(--lp-small-2-extrabold)' }}>Small 2 - ExtraBold</span>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <label style={{ font: 'var(--lp-label-1-regular)', display: 'block' }}>
+            Label 1 - Regular
+          </label>
+          <label style={{ font: 'var(--lp-label-1-medium)', display: 'block' }}>
+            Label 1 - Medium
+          </label>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <label style={{ font: 'var(--lp-label-2-regular)', display: 'block' }}>
+            Label 2 - Regular
+          </label>
+          <label style={{ font: 'var(--lp-label-2-medium)', display: 'block' }}>
+            Label 2 - Medium
+          </label>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--lp-spacing-600)' }}>
+          <code style={{ font: 'var(--lp-code-1-regular)', display: 'block' }}>
+            Code 1 - Regular
+          </code>
+          <code style={{ font: 'var(--lp-code-2-regular)', display: 'block' }}>
+            Code 2 - Regular
+          </code>
+        </div>
+      </div>
+    );
+  },
+};
