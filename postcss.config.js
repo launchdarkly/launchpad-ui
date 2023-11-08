631fa5fb85bc2ffaa501d5e793ead2f668f5e628
Robb Niznik <rniznik@launchdarkly.com>
2023-11-08 07:25:01 -0800

    fix: resolve queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
     require('postcss-preset-env'),
     require('@csstools/postcss-global-data')({
-      files: ['packages/tokens/dist/media-queries.css'],
+      files: [path.resolve(__dirname, './packages/tokens/dist/media-queries.css')],
     }),
     require('postcss-custom-media'),
   ],
