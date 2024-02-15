53b3cf331df22cc1bfa5cf982f060869c6d7f425
Robb Niznik <rniznik@launchdarkly.com>
2024-02-15 09:26:07 -0500

    feat(components): add `SubmenuTrigger`

--- packages/components/stories/Menu.stories.tsx
+++ packages/components/stories/Menu.stories.tsx
@@ -3,13 +3,14 @@ import type { Meta, StoryFn, StoryObj } from '@storybook/react';
 import type { Selection as AriaSelection } from 'react-aria-components';
 
 import { Icon } from '@launchpad-ui/icons';
-import { expect, userEvent, within } from '@storybook/test';
+import { expect, userEvent, within, waitFor } from '@storybook/test';
 import { useState } from 'react';
 
 import {
   Menu,
   MenuItem,
   MenuTrigger,
+  SubmenuTrigger,
   Header,
   Keyboard,
   Section,
@@ -47,7 +48,9 @@ const renderMenu = (args: Story['args']) => (
     <Popover>
       <Menu {...args}>
         <MenuItem>Item one</MenuItem>
-        <MenuItem>Item two</MenuItem>
+        <MenuItem>
+          <Text slot="label">Item two</Text>
+        </MenuItem>
         <MenuItem>Item three</MenuItem>
       </Menu>
     </Popover>
@@ -189,5 +192,54 @@ export const States: Story = {
     const menuitems = await within(body).findAllByRole('menuitem');
     await userEvent.pointer([{ keys: '[TouchA>]', target: menuitems[1] }]);
     await userEvent.keyboard('{arrowdown}');
+  },
+};
+
+export const Submenus: Story = {
+  render: (args) => {
+    return (
+      <MenuTrigger>
+        <Button>Trigger</Button>
+        <Popover>
+          <Menu {...args}>
+            <MenuItem>Item one</MenuItem>
+            <MenuItem>Item two</MenuItem>
+            <MenuItem>Item three</MenuItem>
+            <SubmenuTrigger>
+              <MenuItem>Item four</MenuItem>
+              <Popover>
+                <Menu {...args}>
+                  <MenuItem>Item five</MenuItem>
+                  <MenuItem>Item six</MenuItem>
+                  <SubmenuTrigger>
+                    <MenuItem>Item seven</MenuItem>
+                    <Popover>
+                      <Menu {...args}>
+                        <MenuItem>Item eight</MenuItem>
+                        <MenuItem>Item nine</MenuItem>
+                      </Menu>
+                    </Popover>
+                  </SubmenuTrigger>
+                </Menu>
+              </Popover>
+            </SubmenuTrigger>
+          </Menu>
+        </Popover>
+      </MenuTrigger>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.click(canvas.getByRole('button'));
+    const body = canvasElement.ownerDocument.body;
+    const items = await within(body).findAllByRole('menuitem');
+    await userEvent.hover(items[items.length - 1]);
+
+    await waitFor(async () => {
+      await expect(await within(body).findAllByRole('menu')).toHaveLength(2);
+    });
+    const subItems = await within(body).findAllByRole('menuitem');
+    await userEvent.hover(subItems[subItems.length - 1]);
   },
 };
