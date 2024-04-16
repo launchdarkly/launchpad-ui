19aa22a9a032d15cac6c878d96e93bf95a2df8d5
Robert Niznik <rniznik@launchdarkly.com>
2024-04-16 12:01:56 -0400

    fix(components): update menu styles (#1245)
    * fix(components): update menu styles
    
    * fix: update usage of sections

--- packages/components/src/Separator.tsx
+++ packages/components/src/Separator.tsx
@@ -1,6 +1,19 @@
+import type { ForwardedRef } from 'react';
 import type { SeparatorProps } from 'react-aria-components';
 
-import { Separator } from 'react-aria-components';
+import { cva } from 'class-variance-authority';
+import { forwardRef } from 'react';
+import { Separator as AriaSeparator } from 'react-aria-components';
+
+import styles from './styles/Separator.module.css';
+
+const separator = cva(styles.separator);
+
+const _Separator = ({ className, ...props }: SeparatorProps, ref: ForwardedRef<HTMLElement>) => {
+	return <AriaSeparator {...props} ref={ref} className={separator({ className })} />;
+};
+
+const Separator = forwardRef(_Separator);
 
 export { Separator };
 export type { SeparatorProps };
