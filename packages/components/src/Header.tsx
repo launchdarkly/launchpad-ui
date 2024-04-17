8cae901ef0f8ccd9fe041d66dce1a88ebde2d54d
Robert Niznik <rniznik@launchdarkly.com>
2024-04-17 11:18:58 -0400

    fix(components): extract `Header` styles (#1246)
    * fix(components): extract `Header` styles
    
    * fix: remove overflow

--- packages/components/src/Header.tsx
+++ packages/components/src/Header.tsx
@@ -1,3 +1,20 @@
-import { Header } from 'react-aria-components';
+import type { ForwardedRef, HTMLAttributes } from 'react';
+
+import { cva } from 'class-variance-authority';
+import { forwardRef } from 'react';
+import { Header as AriaHeader } from 'react-aria-components';
+
+import styles from './styles/Header.module.css';
+
+const header = cva(styles.header);
+
+const _Header = (
+	{ className, ...props }: HTMLAttributes<HTMLElement>,
+	ref: ForwardedRef<HTMLElement>,
+) => {
+	return <AriaHeader {...props} ref={ref} className={header({ className })} />;
+};
+
+const Header = forwardRef(_Header);
 
 export { Header };
