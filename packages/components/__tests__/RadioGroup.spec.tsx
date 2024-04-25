8b6aedf181d31c98be789cc3e21f35343338fb9c
Robb Niznik <rniznik@launchdarkly.com>
2024-04-25 10:42:55 -0400

    test: bump coverage

--- packages/components/__tests__/RadioGroup.spec.tsx
+++ packages/components/__tests__/RadioGroup.spec.tsx
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest';
 
 import { render, screen } from '../../../test/utils';
-import { Label, Radio, RadioGroup } from '../src';
+import { ButtonGroup, Label, Radio, RadioButton, RadioGroup } from '../src';
 
 describe('Radio', () => {
 	it('renders', () => {
@@ -20,9 +20,11 @@ describe('Radio', () => {
 		render(
 			<RadioGroup defaultValue="2">
 				<Label>Label</Label>
-				<Radio value="1">First</Radio>
-				<Radio value="2">Second</Radio>
-				<Radio value="3">Third</Radio>
+				<ButtonGroup spacing="compact" role="presentation">
+					<RadioButton value="1">First</RadioButton>
+					<RadioButton value="2">Second</RadioButton>
+					<RadioButton value="3">Third</RadioButton>
+				</ButtonGroup>
 			</RadioGroup>,
 		);
 		expect(screen.getByRole('radiogroup')).toBeVisible();
