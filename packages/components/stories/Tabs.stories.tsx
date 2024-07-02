1d87de22383be092db17c73166cbe08512db0ac9
Robert Niznik <rniznik@launchdarkly.com>
2024-07-02 14:41:11 -0400

    feat(components): add `Tabs` (#1333)

---
+++ packages/components/stories/Tabs.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Tab, TabList, TabPanel, Tabs } from '../src';
+
+import { userEvent, within } from '@storybook/test';
+
+const meta: Meta<typeof Tabs> = {
+	component: Tabs,
+	// @ts-ignore
+	subcomponents: { Tab, TabList, TabPanel },
+	title: 'Components/Navigation/Tabs',
+	parameters: {
+		status: {
+			type: import.meta.env.STORYBOOK_PACKAGE_STATUS__COMPONENTS,
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Tabs>;
+
+export const Example: Story = {
+	args: {
+		children: (
+			<>
+				<TabList aria-label="tabs">
+					<Tab id="1">Tab 1</Tab>
+					<Tab id="2">Tab 2</Tab>
+					<Tab id="3">Tab 3</Tab>
+				</TabList>
+				<TabPanel id="1">Tab body 1</TabPanel>
+				<TabPanel id="2">Tab body 2</TabPanel>
+				<TabPanel id="3">Tab body 3</TabPanel>
+			</>
+		),
+	},
+};
+
+export const States: Story = {
+	args: {
+		children: (
+			<>
+				<TabList aria-label="tabs">
+					<Tab id="1">Focus Visible</Tab>
+					<Tab id="2" isDisabled>
+						Disabled
+					</Tab>
+					<Tab id="3">Hover</Tab>
+				</TabList>
+				<TabPanel id="1">Tab body 1</TabPanel>
+				<TabPanel id="2">Tab body 2</TabPanel>
+				<TabPanel id="3">Tab body 3</TabPanel>
+			</>
+		),
+	},
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const body = canvasElement.ownerDocument.body;
+		body.click();
+		await userEvent.tab();
+
+		const tabs = canvas.getAllByRole('tab');
+		await userEvent.hover(tabs[2]);
+	},
+};
