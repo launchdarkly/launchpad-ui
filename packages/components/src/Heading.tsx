183640d901ff26ac878a5f53e0b8257d64ce6882
Robb Niznik <rniznik@launchdarkly.com>
2024-10-02 16:33:16 -0400

    feat(components): add `Disclosure`

--- packages/components/src/Heading.tsx
+++ packages/components/src/Heading.tsx
@@ -1,6 +1,19 @@
+import type { ForwardedRef } from 'react';
 import type { HeadingProps } from 'react-aria-components';
 
-import { Heading } from 'react-aria-components';
+import { cva } from 'class-variance-authority';
+import { forwardRef } from 'react';
+import { Heading as AriaHeading } from 'react-aria-components';
+
+import styles from './styles/Heading.module.css';
+
+const heading = cva(styles.heading);
+
+const _Heading = ({ className, ...props }: HeadingProps, ref: ForwardedRef<HTMLHeadingElement>) => {
+	return <AriaHeading {...props} ref={ref} className={heading({ className })} />;
+};
+
+const Heading = forwardRef(_Heading);
 
 export { Heading };
 export type { HeadingProps };
