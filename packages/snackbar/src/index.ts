f18898f08a38e9208e33e9fc723a483ae8e491d8
Robb Niznik <rniznik@launchdarkly.com>
2024-10-03 10:52:23 -0400

    chore: remove data-table, slider, snackbar, toast, and toggle packages

--- packages/snackbar/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type { SnackbarProps } from './Snackbar';
-export type { SnackbarCenterProps, SnackbarRecord } from './SnackbarCenter';
-export { Snackbar } from './Snackbar';
-export { SnackbarCenter } from './SnackbarCenter';
