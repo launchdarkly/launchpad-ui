b538443670a518a602bfe56df3ff326d116017cd
Robert Niznik <rniznik@launchdarkly.com>
2024-10-29 15:24:05 -0400

    feat(components): add `Disclosure` (#1430)
    * feat(components): add `Disclosure`
    
    * chore: add changeset
    
    * feat: add accordion-like story
    
    * fix: style like card

---
+++ packages/components/src/Disclosure.tsx
@@ -0,0 +1,45 @@
+import type { ForwardedRef } from 'react';
+import type { DisclosurePanelProps, DisclosureProps } from 'react-aria-components';
+
+import { cva } from 'class-variance-authority';
+import { forwardRef } from 'react';
+import {
+	UNSTABLE_Disclosure as AriaDisclosure,
+	UNSTABLE_DisclosurePanel as AriaDisclosurePanel,
+} from 'react-aria-components';
+
+import styles from './styles/Disclosure.module.css';
+
+const disclosure = cva(styles.disclosure);
+const panel = cva(styles.panel);
+
+const _Disclosure = (
+	{ className, ...props }: DisclosureProps,
+	ref: ForwardedRef<HTMLDivElement>,
+) => {
+	return <AriaDisclosure {...props} ref={ref} className={disclosure({ className })} />;
+};
+
+/**
+ * A disclosure is a collapsible section of content. It is composed of a header with a heading and trigger button, and a panel that contains the content.
+ *
+ * https://react-spectrum.adobe.com/react-aria/Disclosure.html
+ */
+const Disclosure = forwardRef(_Disclosure);
+
+const _DisclosurePanel = (
+	{ className, ...props }: DisclosurePanelProps,
+	ref: ForwardedRef<HTMLDivElement>,
+) => {
+	return <AriaDisclosurePanel {...props} ref={ref} className={panel({ className })} />;
+};
+
+/**
+ * A DisclosurePanel provides the content for a disclosure.
+ *
+ * https://react-spectrum.adobe.com/react-aria/Disclosure.html
+ */
+const DisclosurePanel = forwardRef(_DisclosurePanel);
+
+export { Disclosure, DisclosurePanel };
+export type { DisclosureProps, DisclosurePanelProps };
---
+++ packages/components/stories/Disclosure.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Icon } from '@launchpad-ui/icons';
+import { vars } from '@launchpad-ui/vars';
+
+import { Button, Disclosure, DisclosurePanel, Heading } from '../src';
+
+const meta: Meta<typeof Disclosure> = {
+	component: Disclosure,
+	// @ts-ignore
+	subcomponents: { DisclosurePanel },
+	title: 'Components/Navigation/Disclosure',
+	parameters: {
+		status: {
+			type: import.meta.env.STORYBOOK_PACKAGE_STATUS__COMPONENTS,
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Disclosure>;
+
+export const Example: Story = {
+	args: {
+		children: (
+			<>
+				<Heading>
+					<Button slot="trigger" variant="minimal">
+						<Icon name="chevron-right" size="small" />
+						Header
+					</Button>
+				</Heading>
+				<DisclosurePanel>
+					<p>Panel with content.</p>
+				</DisclosurePanel>
+			</>
+		),
+	},
+};
+
+export const Accordion: Story = {
+	args: {
+		children: (
+			<>
+				<Heading>
+					<Button
+						slot="trigger"
+						style={{ padding: vars.spacing[400], width: '100%', justifyContent: 'flex-start' }}
+						variant="minimal"
+					>
+						<Icon name="chevron-right" size="small" />
+						Header
+					</Button>
+				</Heading>
+				<DisclosurePanel>
+					<p>Panel with content.</p>
+				</DisclosurePanel>
+			</>
+		),
+		style: {
+			border: `1px solid ${vars.color.border.interactive.secondary.base}`,
+			borderRadius: vars.borderRadius.medium,
+		},
+	},
+};
