e18b6d1547b6c1c4aff569549fb0315e0312d7c0
Robb Niznik <rniznik@launchdarkly.com>
2024-11-19 10:56:10 -0500

    feat(components): add `Perceivable` to support `aria-disabled` for RAC

---
+++ packages/components/src/Perceivable.tsx
@@ -0,0 +1,37 @@
+import type { KeyboardEvents, PressEvents } from '@react-types/shared';
+import type { ReactNode } from 'react';
+
+import { FocusableProvider } from '@react-aria/focus';
+import { createContext } from 'react';
+import { Provider } from 'react-aria-components';
+
+interface InteractionProps extends KeyboardEvents, PressEvents {}
+
+interface PerceivableProps {
+	children: ReactNode;
+}
+
+const PerceivableContext = createContext<InteractionProps>({});
+
+const Perceivable = ({ children }: PerceivableProps) => {
+	const props = {
+		onPress: undefined,
+		onPressStart: undefined,
+		onPressEnd: undefined,
+		onPressChange: undefined,
+		onPressUp: undefined,
+		onKeyDown: undefined,
+		onKeyUp: undefined,
+		onClick: undefined,
+		href: undefined,
+	};
+
+	return (
+		<Provider values={[[PerceivableContext, { ...props }]]}>
+			<FocusableProvider aria-disabled="true">{children}</FocusableProvider>
+		</Provider>
+	);
+};
+
+export { Perceivable, PerceivableContext };
+export type { PerceivableProps };
