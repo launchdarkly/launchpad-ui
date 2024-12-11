714abcff02a83d92219ccd1a5ac22f390e2efaa7
Robert Niznik <rniznik@launchdarkly.com>
2024-12-11 13:48:38 -0500

    feat(components): add `warning` variant for `Alert` (#1508)

--- packages/components/src/Alert.tsx
+++ packages/components/src/Alert.tsx
@@ -18,6 +18,7 @@ const alert = cva(styles.base, {
 			error: styles.error,
 			info: styles.info,
 			success: styles.success,
+			warning: styles.warning,
 		},
 		variant: {
 			default: styles.default,
