e532df9fe901550a5767cfb4c741f34ab569bc03
Matthew Ferry <mferry@launchdarkly.com>
2025-02-07 06:03:54 +0000

    chore: update Radio style and svg

--- packages/components/src/Radio.tsx
+++ packages/components/src/Radio.tsx
@@ -1,7 +1,6 @@
 import type { Ref } from 'react';
 import type { RadioProps as AriaRadioProps, RadioRenderProps } from 'react-aria-components';
 
-import { Icon } from '@launchpad-ui/icons';
 import { cva } from 'class-variance-authority';
 import { Radio as AriaRadio, composeRenderProps } from 'react-aria-components';
 
@@ -16,7 +15,15 @@ interface RadioProps extends AriaRadioProps {
 
 const RadioInner = ({ isSelected }: Partial<RadioRenderProps>) => (
 	<div className={circle()}>
-		{isSelected ? <Icon name="circle" className={styles.icon} /> : null}
+		{isSelected ? (
+			<svg aria-hidden="true" className={styles.icon} viewBox="0 0 16 16">
+				<path
+					fillRule="evenodd"
+					clipPath="evenodd"
+					d="M6.852 5.228a3 3 0 1 1 2.296 5.544 3 3 0 0 1-2.296-5.544Z"
+				/>
+			</svg>
+		) : null}
 	</div>
 );
 
