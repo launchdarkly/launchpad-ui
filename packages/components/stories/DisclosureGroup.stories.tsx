93d39d1bb02551805932cdc22184fc7c640a0854
Robb Niznik <rniznik@launchdarkly.com>
2025-03-05 15:42:11 -0500

    feat(components): add `DisclosureGroup`

---
+++ packages/components/stories/DisclosureGroup.stories.tsx
@@ -0,0 +1,57 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Icon } from '@launchpad-ui/icons';
+
+import { Button, Disclosure, DisclosureGroup, DisclosurePanel, Heading } from '../src';
+
+const meta: Meta<typeof DisclosureGroup> = {
+	component: DisclosureGroup,
+	title: 'Components/Navigation/DisclosureGroup',
+};
+
+export default meta;
+
+type Story = StoryObj<typeof DisclosureGroup>;
+
+export const Example: Story = {
+	args: {
+		children: (
+			<>
+				<Disclosure id="one">
+					<Heading>
+						<Button slot="trigger" variant="minimal">
+							<Icon name="chevron-right" size="small" />
+							Disclosure one
+						</Button>
+					</Heading>
+					<DisclosurePanel>
+						<p>Panel with content.</p>
+					</DisclosurePanel>
+				</Disclosure>
+				<Disclosure id="two">
+					<Heading>
+						<Button slot="trigger" variant="minimal">
+							<Icon name="chevron-right" size="small" />
+							Disclosure two
+						</Button>
+					</Heading>
+					<DisclosurePanel>
+						<p>Panel with content.</p>
+					</DisclosurePanel>
+				</Disclosure>
+				<Disclosure id="three">
+					<Heading>
+						<Button slot="trigger" variant="minimal">
+							<Icon name="chevron-right" size="small" />
+							Disclosure three
+						</Button>
+					</Heading>
+					<DisclosurePanel>
+						<p>Panel with content.</p>
+					</DisclosurePanel>
+				</Disclosure>
+			</>
+		),
+		defaultExpandedKeys: ['two'],
+	},
+};
