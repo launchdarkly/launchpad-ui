206d91184fe2aa6c48977293cc3122f5f258c36e
Robb Niznik <rniznik@launchdarkly.com>
2025-03-27 13:14:16 -0400

    chore: delete chip, clipboard, and progress packages

--- packages/progress/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type { DelayedIndicatorProps } from './DelayedIndicator';
-export type { ProgressProps } from './Progress';
-export { DelayedIndicator } from './DelayedIndicator';
-export { Progress } from './Progress';
