a899fcac8c2abb5650eb94d33441b9c53556b610
Alexis Georges <alexis.georges@gmail.com>
2025-04-30 16:24:46 -0400

    add story

--- packages/components/stories/ButtonGroup.stories.tsx
+++ packages/components/stories/ButtonGroup.stories.tsx
@@ -3,6 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../src/Button';
 import { ButtonGroup } from '../src/ButtonGroup';
 import { IconButton } from '../src/IconButton';
+import { LinkButton } from '../src/LinkButton';
 import { Menu, MenuItem, MenuTrigger } from '../src/Menu';
 import { Popover } from '../src/Popover';
 
@@ -42,6 +43,26 @@ export const SplitButton: Story = {
 		children: (
 			<>
 				<Button>Split button</Button>
+				<MenuTrigger>
+					<IconButton icon="chevron-down" aria-label="open" />
+					<Popover placement="bottom end">
+						<Menu>
+							<MenuItem>Item one</MenuItem>
+							<MenuItem>Item two</MenuItem>
+						</Menu>
+					</Popover>
+				</MenuTrigger>
+			</>
+		),
+		spacing: 'compact',
+	},
+};
+
+export const SplitLinkButton: Story = {
+	args: {
+		children: (
+			<>
+				<LinkButton>Split link button</LinkButton>
 				<MenuTrigger>
 					<IconButton icon="chevron-down" aria-label="open" />
 					<Popover placement="bottom end">
