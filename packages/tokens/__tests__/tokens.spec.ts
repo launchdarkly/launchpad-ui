7b5fd128c8ff1a284a0110eed313d292a5cd0da0
Robb Niznik <rniznik@launchdarkly.com>
2025-04-30 13:48:57 -0400

    chore: add primitives test

--- packages/tokens/__tests__/tokens.spec.ts
+++ packages/tokens/__tests__/tokens.spec.ts
@@ -2,11 +2,33 @@ import type { DesignTokens } from 'style-dictionary/types';
 
 import path from 'path';
 import StyleDictionary from 'style-dictionary';
+import { formats, transformGroups, transforms } from 'style-dictionary/enums';
 import { describe, expect, it } from 'vitest';
 
 import { css } from '../src/themes';
 import aliasDark from '../tokens/color-aliases.dark.json' with { type: 'json' };
 import aliasDefault from '../tokens/color-aliases.default.json' with { type: 'json' };
+
+const primitives = () => ({
+	source: [path.resolve(__dirname, '../tokens/color-primitives.json')],
+	platforms: {
+		css: {
+			prefix: 'lp',
+			transformGroup: transformGroups.css,
+			transforms: [transforms.colorRgb],
+			options: {
+				outputReferences: true,
+				usesDtcg: true,
+			},
+			files: [
+				{
+					destination: 'primitives.css',
+					format: formats.cssVariables,
+				},
+			],
+		},
+	},
+});
 
 const sameKeys = (tokens1: DesignTokens, tokens2: DesignTokens) => {
 	for (const key in tokens1) {
@@ -27,6 +49,15 @@ const sameKeys = (tokens1: DesignTokens, tokens2: DesignTokens) => {
 describe('Tokens', () => {
 	it('uses default theme as base for dark theme', () => {
 		expect(sameKeys(aliasDark, aliasDefault)).toBeTruthy();
+	});
+
+	it('builds default primitive tokens', async () => {
+		const config = primitives();
+
+		const sd = new StyleDictionary(config);
+		const [file] = await sd.formatPlatform('css');
+
+		expect(file.output).toMatchSnapshot();
 	});
 
 	it('builds default alias tokens', async () => {
