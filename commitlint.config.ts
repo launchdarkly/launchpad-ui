98ecde787111373350b5eff68992730e867afede
PShukla <pshukla@launchdarkly.com>
2025-12-17 21:29:52 +0530

    fix: added overriding rule to fix the body-max-line-length constraint by commitlint
    This change has been done since this is causing the sync icon workflow to fail when the PR body exceeds 100 characters in length. This is happening because of commitlint/config-conventional's 100 character body line length limit

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
 	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'body-max-line-length': [0, 'always', Number.POSITIVE_INFINITY],
+	},
 };
 
 export default Configuration;
