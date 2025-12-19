bce12c3af840c261ddfdd73a5802b743fd9d2659
Aaron Hogue <ahogue@launchdarkly.com>
2025-12-19 11:14:14 -0500

    chore: remove changeset for dev dependency update
    Dev dependency updates don't require changesets as they don't affect published packages

--- .changeset/storybook-security-advisory.md
+++
@@ -1,5 +0,0 @@
----
-Resolves security advisory for Storybook https://www.chromatic.com/blog/storybook-security-advisory/
----
-
-[BIS-2330] Resolve Storybook Security Advisory
--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,10 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
 	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-empty': [0],
+		'type-empty': [0],
+	},
 };
 
 export default Configuration;
